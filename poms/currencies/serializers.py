346033bdd6ad3d9d3e1c3b0e353b1c3c3e2934f8
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-03-11 14:27:09 +0300

    add fx rate runtime evaluator

--- poms/currencies/serializers.py
+++ poms/currencies/serializers.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 from poms.api.fields import CurrentMasterUserDefault
@@ -18,8 +20,19 @@ class CurrencySerializer(serializers.ModelSerializer):
 
 class CurrencyHistorySerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='currencyhistory-detail')
+    fx_rate_expr = serializers.CharField(max_length=50, write_only=True, required=False, allow_null=True,
+                                         help_text=_('Expression to calculate fx rate (for example 1/75)'))
 
     class Meta:
         model = CurrencyHistory
-        fields = ['url', 'id', 'currency', 'date', 'fx_rate']
+        fields = ['url', 'id', 'currency', 'date', 'fx_rate', 'fx_rate_expr']
 
+    def validate(self, data):
+        fx_rate_expr = data.pop('fx_rate_expr', None)
+        if fx_rate_expr:
+            import simpleeval
+            try:
+                data['fx_rate'] = simpleeval.simple_eval(fx_rate_expr)
+            except (simpleeval.InvalidExpression, ArithmeticError) as e:
+                raise serializers.ValidationError({'fx_rate_expr':force_text(e)})
+        return data
