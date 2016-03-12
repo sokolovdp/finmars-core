2aeed21fdd6050510183ea1c0aa5bdfebfa2daaa
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-03-12 18:16:14 +0300

    fix celery config

--- poms_app/__init__.py
+++ poms_app/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
 
 from .celery import app as celery_app
--- poms_app/celery.py
+++ poms_app/celery.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poms_app.settings')
 
-app = Celery('lagoon.webapi')
+app = Celery('poms.backend')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
