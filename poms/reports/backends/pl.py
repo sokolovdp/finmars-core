23be85625974b1654341f577885419c78ab1be7a
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-03-14 21:29:03 +0300

    fix p&l report

--- poms/reports/backends/pl.py
+++ poms/reports/backends/pl.py
@@ -15,10 +15,11 @@ class PLReportBuilder(BalanceReportBuilder):
         super(PLReportBuilder, self).__init__(*args, **kwargs)
         self._filter_date_attr = 'accounting_date'
 
-    def _get_transaction_qs(self):
-        # TODO: When building P&L report we use Accounting Date
-        queryset = super(BalanceReportBuilder, self)._get_transaction_qs()
-        return queryset
+    def get_cash_account(self, transaction, now=None):
+        return None
+
+    def get_instrument_account(self, transaction, now=None):
+        return None
 
     def build(self):
         # super(PLReportBuilder, self).build()
@@ -30,7 +31,7 @@ class PLReportBuilder(BalanceReportBuilder):
         for bi in balance_items:
             if bi.instrument:
                 pli = PLReportInstrument(bi.instrument)
-                items['%s' % pli.pk] = pli
+                items['%s' % bi.instrument.id] = pli
 
                 # summary.principal_with_sign_system_ccy += bi.principal_value_system_ccy
                 # summary.carry_with_sign_system_ccy += bi.accrued_value_system_ccy
@@ -38,16 +39,9 @@ class PLReportBuilder(BalanceReportBuilder):
                 pli.principal_with_sign_system_ccy += bi.principal_value_system_ccy
                 pli.carry_with_sign_system_ccy += bi.accrued_value_system_ccy
 
-        items = [i for i in six.itervalues(items)]
-        items = sorted(items, key=lambda x: x.pk)
-
-        for i in items:
-            i.total_system_ccy = i.principal_with_sign_system_ccy + \
-                                 i.carry_with_sign_system_ccy + \
-                                 i.overheads_with_sign_system_ccy
-
         self.annotate_fx_rates()
 
+        summary = self.instance.summary
         for t in self.transactions:
             t.transaction_class_code = t_class = t.transaction_class.code
             if t_class == TransactionClass.CASH_INFLOW:
@@ -58,31 +52,53 @@ class PLReportBuilder(BalanceReportBuilder):
                 t.principal_with_sign_system_ccy = t.principal_with_sign * t.settlement_currency_fx_rate
                 t.carry_with_sign_system_ccy = t.carry_with_sign * t.settlement_currency_fx_rate
                 t.overheads_with_sign_system_ccy = t.overheads_with_sign * t.settlement_currency_fx_rate
+
+                pli = items['%s' % t.instrument.id]
+                pli.principal_with_sign_system_ccy += t.principal_with_sign_system_ccy
+                pli.carry_with_sign_system_ccy += t.carry_with_sign_system_ccy
+                pli.overheads_with_sign_system_ccy += t.overheads_with_sign_system_ccy
             elif t_class == TransactionClass.INSTRUMENT_PL:
                 t.principal_with_sign_system_ccy = t.principal_with_sign * t.settlement_currency_fx_rate
                 t.carry_with_sign_system_ccy = t.carry_with_sign * t.settlement_currency_fx_rate
                 t.overheads_with_sign_system_ccy = t.overheads_with_sign * t.settlement_currency_fx_rate
 
+                pli = items['%s' % t.instrument.id]
+                pli.principal_with_sign_system_ccy += t.principal_with_sign_system_ccy
+                pli.carry_with_sign_system_ccy += t.carry_with_sign_system_ccy
+                pli.overheads_with_sign_system_ccy += t.overheads_with_sign_system_ccy
+
             # summary.principal_with_sign_system_ccy += getattr(t, 'principal_with_sign_system_ccy', 0.)
             # summary.carry_with_sign_system_ccy += getattr(t, 'carry_with_sign_system_ccy', 0.)
             # summary.overheads_with_sign_system_ccy += getattr(t, 'overheads_with_sign_system_ccy', 0.)
+            pass
 
-        summary = self.instance.summary
 
-        summary.principal_with_sign_system_ccy = \
-            reduce(lambda x, y: x + y.principal_with_sign_system_ccy, items, 0.) + \
-            reduce(lambda x, y: x + getattr(y, 'principal_with_sign_system_ccy', 0.), self.transactions, 0.)
+        # summary.principal_with_sign_system_ccy = \
+        #     reduce(lambda x, y: x + y.principal_with_sign_system_ccy, items, 0.) + \
+        #     reduce(lambda x, y: x + getattr(y, 'principal_with_sign_system_ccy', 0.), self.transactions, 0.)
+        #
+        # summary.carry_with_sign_system_ccy = \
+        #     reduce(lambda x, y: x + y.carry_with_sign_system_ccy, items, 0.) + \
+        #     reduce(lambda x, y: x + getattr(y, 'carry_with_sign_system_ccy', 0.), self.transactions, 0.)
+        #
+        # summary.overheads_with_sign_system_ccy = \
+        #     reduce(lambda x, y: x + getattr(y, 'overheads_with_sign_system_ccy', 0.), self.transactions, 0.)
 
-        summary.carry_with_sign_system_ccy = \
-            reduce(lambda x, y: x + y.carry_with_sign_system_ccy, items, 0.) + \
-            reduce(lambda x, y: x + getattr(y, 'carry_with_sign_system_ccy', 0.), self.transactions, 0.)
+        # summary.total_system_ccy = summary.principal_with_sign_system_ccy + \
+        #                            summary.carry_with_sign_system_ccy + \
+        #                            summary.overheads_with_sign_system_ccy
 
-        summary.overheads_with_sign_system_ccy = \
-            reduce(lambda x, y: x + getattr(y, 'overheads_with_sign_system_ccy', 0.), self.transactions, 0.)
+        items = [i for i in six.itervalues(items)]
+        items = sorted(items, key=lambda x: x.pk)
+        for i in items:
+            i.total_system_ccy = i.principal_with_sign_system_ccy + \
+                                 i.carry_with_sign_system_ccy + \
+                                 i.overheads_with_sign_system_ccy
 
-        summary.total_system_ccy = summary.principal_with_sign_system_ccy + \
-                                   summary.carry_with_sign_system_ccy + \
-                                   summary.overheads_with_sign_system_ccy
+            summary.principal_with_sign_system_ccy += i.principal_with_sign_system_ccy
+            summary.carry_with_sign_system_ccy += i.carry_with_sign_system_ccy
+            summary.overheads_with_sign_system_ccy += i.overheads_with_sign_system_ccy
+            summary.total_system_ccy += i.total_system_ccy
 
         self.instance.items = items
         self.instance.transactions = self.transactions
