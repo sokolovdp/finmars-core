0432c0e86541b26cd7ab82d8c6733cc79361a89b
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-03-23 12:12:17 +0300

    fix str for notification

--- poms/notifications/models.py
+++ poms/notifications/models.py
@@ -72,11 +72,11 @@ class Notification(models.Model):
         }
         if self.target:
             if self.action_object:
-                return '%(actor)s %(verb)s %(action_object)s on %(target)s %(timesince)s ago' % ctx
-            return '%(actor)s %(verb)s %(target)s %(timesince)s ago' % ctx
+                return '%(actor)s %(verb)s %(action_object)s on %(target)s %(timesince)s' % ctx
+            return '%(actor)s %(verb)s %(target)s %(timesince)s' % ctx
         if self.action_object:
-            return '%(actor)s %(verb)s %(action_object)s %(timesince)s ago' % ctx
-        return '%(actor)s %(verb)s %(timesince)s ago' % ctx
+            return '%(actor)s %(verb)s %(action_object)s %(timesince)s' % ctx
+        return '%(actor)s %(verb)s %(timesince)s' % ctx
 
     def timesince(self, now=None):
         # from django.utils.timesince import timesince as timesince_
