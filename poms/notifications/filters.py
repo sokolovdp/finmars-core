d22e4285383270ab318cbec4c6b71175f2f11fb4
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-03-24 10:49:52 +0300

    refactor

---
+++ poms/notifications/filters.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from rest_framework.filters import BaseFilterBackend
+
+
+class OwnerByRecipientFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        queryset = queryset.filter(recipient=request.user)
+        if request.GET.get('all') in ['1', 'true']:
+            return queryset
+        else:
+            return queryset.filter(read_date__isnull=True)
--- poms/notifications/views.py
+++ poms/notifications/views.py
@@ -2,35 +2,29 @@ from __future__ import unicode_literals
 
 import django_filters
 from django.utils import timezone
+from django_filters.widgets import BooleanWidget
 from rest_framework.decorators import list_route, detail_route
-from rest_framework.filters import DjangoFilterBackend, OrderingFilter, SearchFilter, BaseFilterBackend, FilterSet
+from rest_framework.filters import DjangoFilterBackend, OrderingFilter, SearchFilter, FilterSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from poms.api.mixins import DbTransactionMixin
+from poms.notifications.filters import OwnerByRecipientFilter
 from poms.notifications.models import Notification
 from poms.notifications.serializers import NotificationSerializer
 
 
-class MyNotificationFilter(BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        queryset = queryset.filter(recipient=request.user)
-        if request.GET.get('all') in ['1', 'true']:
-            return queryset
-        else:
-            return queryset.filter(read_date__isnull=True)
-
-
 class NotificationFilter(FilterSet):
-    all = django_filters.MethodFilter(action='show_all')
-    verb = django_filters.CharFilter(lookup_type='contains')
+    all = django_filters.MethodFilter(action='show_all', widget=BooleanWidget())
+    level = django_filters.MultipleChoiceFilter(choices=Notification.LEVELS)
 
     class Meta:
         model = Notification
-        fields = ['all', 'level', 'verb']
+        fields = ['all', 'level']
 
     def show_all(self, qs, value):
+        # see OwnerByRecipientFilter
         return qs
 
 
@@ -38,9 +32,9 @@ class NotificationViewSet(DbTransactionMixin, ReadOnlyModelViewSet):
     queryset = Notification.objects.filter(deleted=False)
     serializer_class = NotificationSerializer
     permission_classes = (IsAuthenticated,)
-    filter_backends = (MyNotificationFilter, DjangoFilterBackend, OrderingFilter, SearchFilter,)
+    filter_backends = (OwnerByRecipientFilter, DjangoFilterBackend, OrderingFilter, SearchFilter,)
     filter_class = NotificationFilter
-    ordering_fields = ['level', 'timestamp']
+    ordering_fields = ['level', 'create_date']
 
     # search_fields = ['verb']
 
