80aab1a0e933ef83c459efe09001ed3ce5dfaba1
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-03-25 21:14:49 +0300

    return full version info only for concrete version id

--- poms/audit/mixins.py
+++ poms/audit/mixins.py
@@ -1,25 +1,29 @@
 from __future__ import unicode_literals
 
-from django.utils.functional import cached_property
 from django.utils.translation import ugettext as _
 from rest_framework import permissions
 from rest_framework.decorators import detail_route, list_route
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from reversion import revisions as reversion
 
 from poms.audit import history
 from poms.audit.models import VersionInfo, ModelProxy
-from poms.audit.pagination import HistoricalPageNumberPagination
 from poms.audit.serializers import VersionSerializer
 from poms.users.fields import get_master_user
+
+
+# class HistoricalPageNumberPagination(PageNumberPagination):
+#     page_size_query_param = 'size'
+#     page_size = 5
+#     max_page_size = 10
 
 
 # TODO: request is to hard for DB, can't prefetch or any optimization
 class HistoricalMixin(object):
     ignore_duplicate_revisions = False
     history_latest_first = True
-    history_pagination_class = HistoricalPageNumberPagination
+
+    # history_pagination_class = HistoricalPageNumberPagination
 
     def dispatch(self, request, *args, **kwargs):
         self._reversion_is_active = False
@@ -46,23 +50,26 @@ class HistoricalMixin(object):
     @list_route()
     def deleted(self, request, pk=None):
         master_user = get_master_user(request)
-        if master_user is None:
-            raise PermissionDenied()
 
         model = self.get_queryset().model
         deleted_list = reversion.get_deleted(model).filter(revision__info__master_user=master_user)
-        return self._make_historical_reponse(model, deleted_list)
+
+        self._version_id = request.query_params.get('version_id')
+        if self._version_id:
+            deleted_list = deleted_list.filter(pk=self._version_id)
+
+        return self._make_historical_reponse(deleted_list)
 
     @detail_route()
     def history(self, request, pk=None):
-        master_user = get_master_user(request)
-        if master_user is None:
-            raise PermissionDenied()
-
         instance = self.get_object()
         version_list = reversion.get_for_object(instance)
-        model = self.get_queryset().model
-        return self._make_historical_reponse(model, version_list)
+
+        self._version_id = request.query_params.get('version_id')
+        if self._version_id:
+            version_list = version_list.filter(pk=self._version_id)
+
+        return self._make_historical_reponse(version_list)
 
     def _get_fields(self, model):
         fields = [field for field in model._meta.fields]
@@ -70,73 +77,55 @@ class HistoricalMixin(object):
         fields += concrete_model._meta.many_to_many
         return fields
 
-    def _historical_annotate_object(self, model, fields, versions):
-        for v in versions:
-            # print('-'*79)
-            # print(v.serialized_data)
-            # print(v.field_dict)
+    def _historical_annotate_object(self, version):
+        # TODO: load one-to-one from history, currently loaded from db
+        # TODO: show many-to-many from history, currently loaded from db
+        if version and self._version_id:
+            serializer = self.get_serializer(instance=ModelProxy(version))
+            version.object_json = serializer.data
 
-            # TODO: load one-to-one from history, currently loaded from db
-            # TODO: show many-to-many from history, currently loaded from db
+    def _historical_annotate_objects(self, versions):
+        if self._version_id:
+            for v in versions:
+                self._historical_annotate_object(v)
 
-            # deser_obj = v.object_version
-            # obj = deser_obj.object
-
-            # if deser_obj.m2m_data:
-            #     for accessor_name, object_list in deser_obj.m2m_data.items():
-            #         setattr(self.object, accessor_name, object_list)
-            # obj = v.object_version.object # deserialize m2m as current value :(
-
-            # obj = model()
-            # for field in fields:
-            #     # print(repr(field))
-            #     setattr(obj, field.name, v.field_dict.get(field.name, None))
-
-            # profile = UserProfile.objects.get(user=obj.pk)
-            # try:
-            #     profile = reversion.get_for_date(profile, v.revision.date_created)
-            #     profile = profile.object_version.object
-            #     obj.profile = profile
-            # except ObjectDoesNotExist:
-            #     obj.profile = UserProfile()
-
-            # serializer = self.get_serializer(instance=obj)
-            serializer = self.get_serializer(instance=ModelProxy(v))
-            v.object_json = serializer.data
-
-    def _make_historical_reponse(self, model, versions):
-        fields = self._get_fields(model)
+    def _make_historical_reponse(self, versions):
         queryset = versions.select_related('content_type', 'revision__user').prefetch_related('revision__info')
+
+        if self._version_id:
+            version = queryset.first()
+            self._historical_annotate_object(version)
+            serializer = VersionSerializer(version)
+            return Response(serializer.data)
 
         if self.history_latest_first:
             queryset = queryset.order_by("-pk")
         else:
             queryset = queryset.order_by("pk")
 
-        page = self._historical_paginate_queryset(queryset)
+        page = self.paginate_queryset(queryset)
         if page is not None:
-            self._historical_annotate_object(model, fields, page)
-
+            self._historical_annotate_objects(page)
             serializer = VersionSerializer(page, many=True)
-            return self._historical_get_paginated_response(serializer.data)
+            return self.get_paginated_response(serializer.data)
 
-        self._historical_annotate_object(model, fields, queryset)
+        self._historical_annotate_objects(queryset)
         serializer = VersionSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    @cached_property
-    def _historical_paginator(self):
-        if self.history_pagination_class:
-            return self.history_pagination_class()
-        return self.pagination_class()
-
-    def _historical_paginate_queryset(self, queryset):
-        if self._historical_paginator is None:
-            return None
-        return self._historical_paginator.paginate_queryset(queryset, self.request, view=self)
-
-        # return self.paginate_queryset(queryset)
-
-    def _historical_get_paginated_response(self, data):
-        assert self._historical_paginator is not None
-        return self._historical_paginator.get_paginated_response(data)
+        # @cached_property
+        # def _historical_paginator(self):
+        #     if self.history_pagination_class:
+        #         return self.history_pagination_class()
+        #     return self.pagination_class()
+        #
+        # def _historical_paginate_queryset(self, queryset):
+        #     if self._historical_paginator is None:
+        #         return None
+        #     return self._historical_paginator.paginate_queryset(queryset, self.request, view=self)
+        #
+        #     # return self.paginate_queryset(queryset)
+        #
+        # def _historical_get_paginated_response(self, data):
+        #     assert self._historical_paginator is not None
+        #     return self._historical_paginator.get_paginated_response(data)
