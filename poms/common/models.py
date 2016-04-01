49571129c2f13ba945a954b0db4d4566ea82d4fd
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-04-01 09:45:03 +0300

    add common package for store common models and fields

---
+++ poms/common/models.py
@@ -0,0 +1,50 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.text import Truncator
+from django.utils.translation import ugettext_lazy as _
+
+
+@python_2_unicode_compatible
+class NamedModel(models.Model):
+    user_code = models.CharField(max_length=25, null=True, blank=True, verbose_name=_('user code'))
+    name = models.CharField(max_length=255, verbose_name=_('name'))
+    short_name = models.CharField(max_length=50, null=True, blank=True, verbose_name=_('short name'))
+    notes = models.TextField(null=True, blank=True, verbose_name=_('notes'))
+
+    class Meta:
+        abstract = True
+        unique_together = [
+            ['master_user', 'user_code']
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.user_code:
+            self.user_code = Truncator(self.name).chars(25)
+        if not self.short_name:
+            self.short_name = Truncator(self.name).chars(50)
+        super(NamedModel, self).save(force_insert=force_insert, force_update=force_update, using=using,
+                                     update_fields=update_fields)
+
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('created'))
+    modified = models.DateTimeField(auto_now=True, editable=False, verbose_name=_('modified'))
+
+    class Meta:
+        abstract = True
+        get_latest_by = 'modified'
+        ordering = ('-modified', '-created',)
+
+
+class UserTimeStampedModel(TimeStampedModel):
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, verbose_name=_('created by'))
+    modified_by = models.DateTimeField(settings.AUTH_USER_MODEL, null=True, blank=True, verbose_name=_('modified by'))
+
+    class Meta:
+        abstract = True
