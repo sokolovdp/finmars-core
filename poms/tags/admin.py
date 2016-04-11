d46641722537d6099c481b237ee9368363423a7e
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-04-11 14:34:00 +0300

    non generic tags

--- poms/tags/admin.py
+++ poms/tags/admin.py
@@ -3,15 +3,38 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from poms.audit.admin import HistoricalAdmin
-from poms.tags.models import AccountTag, CommonTag, PortfolioTag, InstrumentTag, InstrumentTypeTag, CurrencyTag, \
-    ResponsibleTag, CounterpartyTag, TransactionTypeTag
+from poms.tags.models import Tag2, Tag, TaggedObject
 
-admin.site.register(AccountTag, HistoricalAdmin)
-admin.site.register(CurrencyTag, HistoricalAdmin)
-admin.site.register(InstrumentTypeTag, HistoricalAdmin)
-admin.site.register(InstrumentTag, HistoricalAdmin)
-admin.site.register(CounterpartyTag, HistoricalAdmin)
-admin.site.register(ResponsibleTag, HistoricalAdmin)
-admin.site.register(PortfolioTag, HistoricalAdmin)
-admin.site.register(TransactionTypeTag, HistoricalAdmin)
-admin.site.register(CommonTag, HistoricalAdmin)
+
+# admin.site.register(AccountTag, HistoricalAdmin)
+# admin.site.register(CurrencyTag, HistoricalAdmin)
+# admin.site.register(InstrumentTypeTag, HistoricalAdmin)
+# admin.site.register(InstrumentTag, HistoricalAdmin)
+# admin.site.register(CounterpartyTag, HistoricalAdmin)
+# admin.site.register(ResponsibleTag, HistoricalAdmin)
+# admin.site.register(PortfolioTag, HistoricalAdmin)
+# admin.site.register(TransactionTypeTag, HistoricalAdmin)
+
+
+class TaggedObjectInline(admin.TabularInline):
+    model = TaggedObject
+    extra = 0
+
+
+class TagAdmin(HistoricalAdmin):
+    model = Tag
+    inlines = [TaggedObjectInline]
+
+
+admin.site.register(Tag, TagAdmin)
+
+
+class Tag2Admin(HistoricalAdmin):
+    model = Tag2
+    list_display = ['id', 'master_user', 'name', 'content_type']
+    filter_horizontal = ['accounts', 'currencies', 'instrument_types', 'instruments', 'counterparties', 'responsibles',
+                         'portfolios', 'transaction_types']
+    # 'strategies',
+
+
+admin.site.register(Tag2, Tag2Admin)
--- poms/tags/models.py
+++ poms/tags/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from mptt.fields import TreeManyToManyField
@@ -16,171 +18,187 @@ from poms.transactions.models import TransactionType
 from poms.users.models import MasterUser
 
 
-class AccountTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='account_tags', verbose_name=_('master user'))
-    accounts = models.ManyToManyField(Account, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('account tag')
-        verbose_name_plural = _('account tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_accounttag', 'Can view account tag')
-        ]
+class Tag(TagModelBase):
+    master_user = models.ForeignKey(MasterUser, related_name='tags', verbose_name=_('master user'))
+    content_type = models.ForeignKey(ContentType, null=True, blank=True, related_name='+')
 
 
-class CurrencyTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='currency_tags', verbose_name=_('master user'))
-    currencies = models.ManyToManyField(Currency, blank=True, related_name='tags')
+class TaggedObject(models.Model):
+    tag = models.ForeignKey(Tag)
+    content_type = models.ForeignKey(ContentType, related_name='+')
+    object_id = models.BigIntegerField()
+    content_object = GenericForeignKey()
 
-    class Meta:
-        verbose_name = _('currency tag')
-        verbose_name_plural = _('currency tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_currencytag', 'Can view currency tag')
-        ]
+    def __str__(self):
+        return '%s' % self.content_object
 
 
-class InstrumentTypeTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='instrumenttype_tags', verbose_name=_('master user'))
-    instrument_types = models.ManyToManyField(InstrumentType, blank=True, related_name='tags')
+# class AccountTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='account_tags', verbose_name=_('master user'))
+#     accounts = models.ManyToManyField(Account, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('account tag')
+#         verbose_name_plural = _('account tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_accounttag', 'Can view account tag')
+#         ]
+#
+#
+# class CurrencyTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='currency_tags', verbose_name=_('master user'))
+#     currencies = models.ManyToManyField(Currency, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('currency tag')
+#         verbose_name_plural = _('currency tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_currencytag', 'Can view currency tag')
+#         ]
+#
+#
+# class InstrumentTypeTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='instrumenttype_tags', verbose_name=_('master user'))
+#     instrument_types = models.ManyToManyField(InstrumentType, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('instrument type tag')
+#         verbose_name_plural = _('instrument type tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_instrumenttypetag', 'Can view instrument type tag')
+#         ]
+#
+#
+# class InstrumentTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='instrument_tags', verbose_name=_('master user'))
+#     instruments = models.ManyToManyField(Instrument, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('instrument tag')
+#         verbose_name_plural = _('instrument tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_instrumenttag', 'Can view instrument tag')
+#         ]
+#
+#
+# class CounterpartyTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='counterparty_tags', verbose_name=_('master user'))
+#     counterparties = models.ManyToManyField(Counterparty, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('counterparty tag')
+#         verbose_name_plural = _('counterparty tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_counterpartytag', 'Can view counterparty tag')
+#         ]
+#
+#
+# class ResponsibleTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='responsible_tags', verbose_name=_('master user'))
+#     responsibles = models.ManyToManyField(Responsible, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('responsible tag')
+#         verbose_name_plural = _('responsible tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_responsibletag', 'Can view responsible tag')
+#         ]
+#
+#
+# class PortfolioTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='portfolio_tags', verbose_name=_('master user'))
+#     portfolios = models.ManyToManyField(Portfolio, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('portfolio tag')
+#         verbose_name_plural = _('portfolio tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_portfoliotag', 'Can view portfolio tag')
+#         ]
+#
+#
+# class StrategyTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='strategy_tags', verbose_name=_('master user'))
+#     strategies = TreeManyToManyField(Strategy, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('strategy tag')
+#         verbose_name_plural = _('strategy tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_strategytag', 'Can view strategy tag')
+#         ]
+#
+#
+# class TransactionTypeTag(TagModelBase):
+#     master_user = models.ForeignKey(MasterUser, related_name='transactiontype_tags', verbose_name=_('master user'))
+#     transaction_types = models.ManyToManyField(TransactionType, blank=True, related_name='tags')
+#
+#     class Meta:
+#         verbose_name = _('transaction type tag')
+#         verbose_name_plural = _('transaction type tags')
+#         unique_together = [
+#             ['master_user', 'user_code'],
+#             ['master_user', 'name'],
+#         ]
+#         permissions = [
+#             ('view_transactiontypetag', 'Can view instrument type tag')
+#         ]
 
-    class Meta:
-        verbose_name = _('instrument type tag')
-        verbose_name_plural = _('instrument type tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_instrumenttypetag', 'Can view instrument type tag')
-        ]
 
-
-class InstrumentTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='instrument_tags', verbose_name=_('master user'))
-    instruments = models.ManyToManyField(Instrument, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('instrument tag')
-        verbose_name_plural = _('instrument tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_instrumenttag', 'Can view instrument tag')
-        ]
-
-
-class CounterpartyTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='counterparty_tags', verbose_name=_('master user'))
-    counterparties = models.ManyToManyField(Counterparty, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('counterparty tag')
-        verbose_name_plural = _('counterparty tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_counterpartytag', 'Can view counterparty tag')
-        ]
-
-
-class ResponsibleTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='responsible_tags', verbose_name=_('master user'))
-    responsibles = models.ManyToManyField(Responsible, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('responsible tag')
-        verbose_name_plural = _('responsible tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_responsibletag', 'Can view responsible tag')
-        ]
-
-
-class PortfolioTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='portfolio_tags', verbose_name=_('master user'))
-    portfolios = models.ManyToManyField(Portfolio, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('portfolio tag')
-        verbose_name_plural = _('portfolio tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_portfoliotag', 'Can view portfolio tag')
-        ]
-
-
-class StrategyTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='strategy_tags', verbose_name=_('master user'))
-    strategies = TreeManyToManyField(Strategy, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('strategy tag')
-        verbose_name_plural = _('strategy tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_strategytag', 'Can view strategy tag')
-        ]
-
-
-class TransactionTypeTag(TagModelBase):
-    master_user = models.ForeignKey(MasterUser, related_name='transactiontype_tags', verbose_name=_('master user'))
-    transaction_types = models.ManyToManyField(TransactionType, blank=True, related_name='tags')
-
-    class Meta:
-        verbose_name = _('transaction type tag')
-        verbose_name_plural = _('transaction type tags')
-        unique_together = [
-            ['master_user', 'user_code'],
-            ['master_user', 'name'],
-        ]
-        permissions = [
-            ('view_transactiontypetag', 'Can view instrument type tag')
-        ]
-
-
-class CommonTag(TagModelBase):
+class Tag2(TagModelBase):
     master_user = models.ForeignKey(MasterUser, related_name='common_tags', verbose_name=_('master user'))
+    content_type = models.ForeignKey(ContentType, null=True, blank=True, related_name='+')
 
-    accounts = models.ManyToManyField(Account, blank=True, related_name='common_tags')
-    currencies = models.ManyToManyField(Currency, blank=True, related_name='common_tags')
-    instrument_types = models.ManyToManyField(InstrumentType, blank=True, related_name='common_tags')
-    instruments = models.ManyToManyField(Instrument, blank=True, related_name='common_tags')
-    counterparties = models.ManyToManyField(Counterparty, blank=True, related_name='common_tags')
-    responsibles = models.ManyToManyField(Responsible, blank=True, related_name='common_tags')
-    strategies = TreeManyToManyField(Strategy, blank=True, related_name='common_tags')
-    portfolios = models.ManyToManyField(Portfolio, blank=True, related_name='common_tags')
-    transaction_types = models.ManyToManyField(TransactionType, blank=True, related_name='common_tags')
+    accounts = models.ManyToManyField('accounts.Account', blank=True, related_name='tags2')
+    currencies = models.ManyToManyField('currencies.Currency', blank=True, related_name='tags2')
+    instrument_types = models.ManyToManyField('instruments.InstrumentType', blank=True, related_name='tags2')
+    instruments = models.ManyToManyField('instruments.Instrument', blank=True, related_name='tags2')
+    counterparties = models.ManyToManyField('counterparties.Counterparty', blank=True, related_name='tags2')
+    responsibles = models.ManyToManyField('counterparties.Responsible', blank=True, related_name='tags2')
+    strategies = TreeManyToManyField('strategies.Strategy', blank=True, related_name='tags2')
+    portfolios = models.ManyToManyField('portfolios.Portfolio', blank=True, related_name='tags2')
+    transaction_types = models.ManyToManyField('transactions.TransactionType', blank=True, related_name='tags2')
 
 
-history.register(AccountTag)
-history.register(CurrencyTag)
-history.register(InstrumentTypeTag)
-history.register(InstrumentTag)
-history.register(CounterpartyTag)
-history.register(ResponsibleTag)
-history.register(StrategyTag)
-history.register(PortfolioTag)
-history.register(TransactionTypeTag)
-history.register(CommonTag)
+# history.register(AccountTag)
+# history.register(CurrencyTag)
+# history.register(InstrumentTypeTag)
+# history.register(InstrumentTag)
+# history.register(CounterpartyTag)
+# history.register(ResponsibleTag)
+# history.register(StrategyTag)
+# history.register(PortfolioTag)
+# history.register(TransactionTypeTag)
+history.register(Tag2)
