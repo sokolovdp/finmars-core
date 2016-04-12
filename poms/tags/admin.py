1a200a57b49664c713a8067587a9422dc95bf9fd
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-04-12 12:28:50 +0300

    filter content types

--- poms/tags/admin.py
+++ poms/tags/admin.py
@@ -1,10 +1,18 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
 
+from poms.accounts.models import AccountType, Account
 from poms.audit.admin import HistoricalAdmin
+from poms.counterparties.models import Counterparty, Responsible
+from poms.currencies.models import Currency
+from poms.instruments.models import InstrumentType, Instrument
 from poms.obj_perms.admin import UserObjectPermissionAdmin, GroupObjectPermissionAdmin
+from poms.portfolios.models import Portfolio
+from poms.strategies.models import Strategy
 from poms.tags.models import Tag, TagUserObjectPermission, TagGroupObjectPermission
+from poms.transactions.models import TransactionType
 
 
 class TagAdmin(HistoricalAdmin):
@@ -12,6 +20,26 @@ class TagAdmin(HistoricalAdmin):
     list_display = ['id', 'master_user', 'name']
     filter_horizontal = ['content_types', 'account_types', 'accounts', 'currencies', 'instrument_types', 'instruments',
                          'counterparties', 'responsibles', 'portfolios', 'transaction_types']
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name == 'content_types':
+            qs = kwargs.get('queryset', db_field.remote_field.model.objects)
+            ctypes = [
+                ContentType.objects.get_for_model(AccountType).pk,
+                ContentType.objects.get_for_model(Account).pk,
+                ContentType.objects.get_for_model(Currency).pk,
+                ContentType.objects.get_for_model(InstrumentType).pk,
+                ContentType.objects.get_for_model(Instrument).pk,
+                ContentType.objects.get_for_model(Counterparty).pk,
+                ContentType.objects.get_for_model(Responsible).pk,
+                ContentType.objects.get_for_model(Strategy).pk,
+                ContentType.objects.get_for_model(Portfolio).pk,
+                ContentType.objects.get_for_model(TransactionType).pk,
+            ]
+            kwargs['queryset'] = qs.filter(pk__in=ctypes)
+            # kwargs['queryset'] = qs.annotate(c=Concat('app_label', Value('.'), 'model')). \
+            #     filter(c__in=[])
+        return super(TagAdmin, self).formfield_for_manytomany(db_field, request=request, **kwargs)
 
 
 admin.site.register(Tag, TagAdmin)
