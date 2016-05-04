704021adb01270c6dd392d0c5524480bdbeacd52
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-05-04 10:06:52 +0300

    use slug field for content type

--- poms/tags/fields.py
+++ poms/tags/fields.py
@@ -1,16 +1,34 @@
 from __future__ import unicode_literals
 
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.encoding import smart_text
 
-from poms.api.fields import FilteredPrimaryKeyRelatedField
+from poms.api.fields import FilteredPrimaryKeyRelatedField, FilteredSlugRelatedField
 from poms.tags.filters import TagContentTypeFilter
 from poms.tags.models import Tag
 from poms.users.filters import OwnerByMasterUserFilter
 
 
-class TagContentTypeField(FilteredPrimaryKeyRelatedField):
+class TagContentTypeField(FilteredSlugRelatedField):
     queryset = ContentType.objects
     filter_backends = [TagContentTypeFilter]
+
+    def __init__(self, **kwargs):
+        kwargs['slug_field'] = 'model'
+        super(TagContentTypeField, self).__init__(**kwargs)
+
+    def to_internal_value(self, data):
+        try:
+            app_label, model = data.split('.')
+            return self.get_queryset().get(app_label=app_label, model=model)
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist', slug_name=self.slug_field, value=smart_text(data))
+        except (TypeError, ValueError):
+            self.fail('invalid')
+
+    def to_representation(self, obj):
+        return '%s.%s' % (obj.app_label, obj.model)
 
 
 class TagField(FilteredPrimaryKeyRelatedField):
