7a6696ea6eb8e18d5f34c1b3fe8ceb68805e9b37
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-05-04 16:22:42 +0300

    skip IntegrityError

--- poms/common/utils.py
+++ poms/common/utils.py
@@ -1,12 +1,14 @@
-
 def db_class_check_data(model, verbosity, using):
-    exists = set(model.objects.using(using).values_list('pk', flat=True))
+    from django.db import IntegrityError
 
+    exists = set(model.objects.using(using).values_list('pk', flat=True))
     if verbosity >= 2:
         print('existed transaction classes -> %s' % exists)
-
     for id, name in model.CLASSES:
         if id not in exists:
             if verbosity >= 2:
                 print('create %s class -> %s:%s' % (model._meta.verbose_name, id, name))
-            model.objects.using(using).create(pk=id, system_code=name, name=name, description=name)
+            try:
+                model.objects.using(using).create(pk=id, system_code=name, name=name, description=name)
+            except IntegrityError:
+                pass
