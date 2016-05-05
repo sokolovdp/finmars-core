d008bf8a7f4a6072ab4f576c4c076740ac3824e1
Alexander Ilyukhin <ailyukhin@quadrogroup.ru>
2016-05-05 11:22:23 +0300

    model for store object json

--- poms/audit/models.py
+++ poms/audit/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+import json
+
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -10,11 +13,31 @@ from reversion.models import Revision
 
 @python_2_unicode_compatible
 class AuthLogEntry(models.Model):
-    date = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name=_('create date'))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'))
-    user_ip = models.GenericIPAddressField(null=True, blank=True, verbose_name=_('user ip'))
-    user_agent = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('user agent'))
-    is_success = models.BooleanField(default=False, db_index=True, verbose_name=_('is_success'))
+    date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name=_('create date')
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('user')
+    )
+    user_ip = models.GenericIPAddressField(
+        null=True,
+        blank=True,
+        verbose_name=_('user ip')
+    )
+    user_agent = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name=_('user agent')
+    )
+    is_success = models.BooleanField(
+        default=False,
+        db_index=True,
+        verbose_name=_('is_success')
+    )
 
     class Meta:
         verbose_name = _('authenticate log')
@@ -33,3 +56,68 @@ class VersionInfo(models.Model):
     revision = models.ForeignKey(Revision, related_name='info')
     master_user = models.ForeignKey('users.MasterUser')
     username = models.CharField(max_length=255, null=True, blank=True)
+
+
+@python_2_unicode_compatible
+class HistoryEntry(models.Model):
+    ADDITION = 1
+    CHANGE = 2
+    DELETION = 3
+    FLAG_CHOICES = (
+        (ADDITION, 'Added'),
+        (CHANGE, 'Chaned'),
+        (DELETION, 'Deleted'),
+    )
+
+    master_user = models.ForeignKey(
+        'users.MasterUser'
+    )
+    member = models.ForeignKey(
+        'users.Member',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    member_name = models.CharField(
+        verbose_name=_('member name (if member deleted)')
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        related_name='histories',
+        verbose_name=_('content type')
+    )
+    object_id = models.BigIntegerField(verbose_name=_('object id'))
+    content_object = GenericForeignKey()
+    action_flag = models.PositiveSmallIntegerField(
+        verbose_name=_('action flag')
+    )
+    message = models.TextField(
+        null=True,
+        blank=True
+    )
+    json = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        abstract = True
+        verbose_name = _('history')
+        verbose_name_plural = _('histories')
+        ordering = [
+            '-created'
+        ]
+
+    def __str__(self):
+        return self.message
+
+    def get_data(self):
+        return json.loads(self.json) if self.json else None
+
+    def set_data(self, data):
+        self.json = json.dumps(data, sort_keys=True) if data else None
+
+    data = property(get_data, set_data)
