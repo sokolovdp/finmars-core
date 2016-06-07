dc2382dbd11f22f0fec05d5871f4ed56f5d13c42
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-06-07 09:36:30 +0300

    some expression tests

--- poms/common/formula.py
+++ poms/common/formula.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals, print_function
+
 import datetime
 import pprint
 
@@ -16,6 +18,7 @@ def is_valid(expr):
 
 
 def parse(expr):
+    import ast
     try:
         return ast.parse(expr), None
     except (SyntaxError, ValueError) as e:
@@ -25,19 +28,30 @@ def parse(expr):
 DEFAULT_FUNCTIONS = {
     # 'now': lambda: timezone.now()
     'now': lambda: datetime.datetime.utcnow(),
-    'now2': lambda: '%s' % datetime.datetime.utcnow(),
+    # 'now2': lambda: '%s' % datetime.datetime.utcnow(),
 }
+
+
+def funcs(f, with_default=True):
+    if with_default:
+        res = f.copy()
+        res.update(DEFAULT_FUNCTIONS)
+        return res
+    return f
 
 
 def safe_eval(expr, functions=DEFAULT_FUNCTIONS, names=None):
     import simpleeval
     try:
         v = simpleeval.simple_eval(expr, functions=functions, names=names)
-    except simpleeval.InvalidExpression as e:
+        # v = simpleeval.simple_eval(expr,names=names)
+    except (simpleeval.InvalidExpression, KeyError, AttributeError) as e:
         raise InvalidExpression(e)
     pprint.pprint(v)
     return v
 
 
-# safe_eval('now2()')
-safe_eval('{"a": 1}')
+# safe_eval('now()')
+safe_eval('"a".__class__.__class__()')
+# safe_eval('"-" * 2 ** 2 ** 2 ** 2 ** 2')
+# safe_eval('2 >> 2')
