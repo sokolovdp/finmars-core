05f66a76a7d1484d86cac08c08cbda281a075ca1
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-06-15 17:25:49 +0300

    add missed content types

--- poms/ui/admin.py
+++ poms/ui/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from django.contrib.contenttypes.models import ContentType
 
-from poms.accounts.models import AccountType, Account
 from poms.audit.admin import HistoricalAdmin
-from poms.counterparties.models import Counterparty, Responsible
-from poms.currencies.models import Currency
-from poms.instruments.models import InstrumentType, Instrument
-from poms.portfolios.models import Portfolio
-from poms.strategies.models import Strategy1, Strategy2, Strategy3
-from poms.transactions.models import TransactionType, Transaction
+from poms.ui.filters import LayoutContentTypeFilter
 from poms.ui.models import TemplateListLayout, TemplateEditLayout, ListLayout, EditLayout
 
 
@@ -16,10 +9,7 @@ class BaseLayoutAdmin(HistoricalAdmin):
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
         if db_field.name == 'content_type':
             qs = kwargs.get('queryset', db_field.remote_field.model.objects)
-            models = [AccountType, Account, Currency, InstrumentType, Instrument, Counterparty, Responsible, Portfolio,
-                      TransactionType, Transaction, Strategy1, Strategy2, Strategy3, ]
-            ctypes = [ContentType.objects.get_for_model(model).pk for model in models]
-            kwargs['queryset'] = qs.filter(pk__in=ctypes)
+            kwargs['queryset'] = LayoutContentTypeFilter().filter_queryset(request, qs, None)
         return super(BaseLayoutAdmin, self).formfield_for_foreignkey(db_field, request=request, **kwargs)
 
 
--- poms/ui/filters.py
+++ poms/ui/filters.py
@@ -6,35 +6,21 @@ class LayoutContentTypeFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         from poms.accounts.models import AccountType
         from poms.accounts.models import Account
-        from poms.currencies.models import Currency
-        from poms.instruments.models import InstrumentType
-        from poms.instruments.models import Instrument
+        from poms.currencies.models import Currency, CurrencyHistory
+        from poms.instruments.models import InstrumentType, Instrument, PriceHistory, PricingPolicy
         from poms.counterparties.models import Counterparty
         from poms.counterparties.models import Responsible
         from poms.strategies.models import Strategy1, Strategy2, Strategy3
         from poms.portfolios.models import Portfolio
         from poms.transactions.models import TransactionType, Transaction
 
-        models = [
-                AccountType,
-                Account,
-                Currency,
-                InstrumentType,
-                Instrument,
-                Counterparty,
-                Responsible,
-                Portfolio,
-                TransactionType,
-                Transaction,
-                Strategy1,
-                Strategy2,
-                Strategy3,
-            ]
+        models = [AccountType, Account, Currency, CurrencyHistory, InstrumentType, Instrument, PriceHistory,
+                  PricingPolicy, Counterparty, Responsible, Portfolio, TransactionType, Transaction,
+                  Strategy1, Strategy2, Strategy3, ]
         ctypes = [ContentType.objects.get_for_model(model).pk for model in models]
-        return queryset.filter(pk__in=ctypes)
+        return queryset.filter(pk__in=ctypes).order_by('model')
 
 
 class MyLayoutFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         return queryset.filter(mamber=request.user.member)
-
