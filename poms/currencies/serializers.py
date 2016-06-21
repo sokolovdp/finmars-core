0a23827a46c886fc579c2090fcae463b56839df8
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-06-21 10:29:26 +0300

    fix cuurency history api

--- poms/currencies/serializers.py
+++ poms/currencies/serializers.py
@@ -26,22 +26,21 @@ class CurrencySerializer(ModelWithUserCodeSerializer):
 
 class CurrencyHistorySerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='currencyhistory-detail')
-    master_user = MasterUserField()
     currency = CurrencyField()
     pricing_policy = PricingPolicyField()
-    fx_rate_expr = serializers.CharField(max_length=50, write_only=True, required=False, allow_null=True,
-                                         help_text=_('Expression to calculate fx rate (for example 1/75)'))
+    # fx_rate_expr = serializers.CharField(max_length=50, write_only=True, required=False, allow_null=True,
+    #                                      help_text=_('Expression to calculate fx rate (for example 1/75)'))
 
     class Meta:
         model = CurrencyHistory
-        fields = ['url', 'id', 'master_user', 'currency', 'pricing_policy', 'date', 'fx_rate', 'fx_rate_expr']
-        readonly_fields = ['is_global']
+        fields = ['url', 'id', 'currency', 'pricing_policy', 'date', 'fx_rate']
+        # readonly_fields = ['is_global']
 
-    def validate(self, data):
-        fx_rate_expr = data.pop('fx_rate_expr', None)
-        if fx_rate_expr:
-            try:
-                data['fx_rate'] = formula.safe_eval(fx_rate_expr)
-            except (formula.InvalidExpression, ArithmeticError) as e:
-                raise serializers.ValidationError({'fx_rate_expr': force_text(e)})
-        return data
+    # def validate(self, data):
+    #     fx_rate_expr = data.pop('fx_rate_expr', None)
+    #     if fx_rate_expr:
+    #         try:
+    #             data['fx_rate'] = formula.safe_eval(fx_rate_expr)
+    #         except (formula.InvalidExpression, ArithmeticError) as e:
+    #             raise serializers.ValidationError({'fx_rate_expr': force_text(e)})
+    #     return data
