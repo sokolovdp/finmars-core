8667c60978fbeeca16c431e070586af8b3b6c046
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-06-22 15:05:10 +0300

    keep tests db

--- poms_app/test_settings.py
+++ poms_app/test_settings.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
+
 import os
+
+# OPTIONS: --keepdb
+# ENV    : DJANGO_SETTINGS_MODULE=poms_app.test_settings;PYTHONUNBUFFERED=1
 
 os.environ['DJANGO_DEBUG'] = 'True'
 
@@ -7,7 +11,11 @@ from .settings import *
 
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': ':memory:',
+    # 'NAME': ':memory:',
+    'TEST': {
+        # 'NAME': 'mytestdatabase',
+        'NAME': os.path.join(BASE_DIR, 'db-tests.sqlite3'),
+    },
 }
 
 # LOGGING['loggers']['django.db'] = {'level': 'DEBUG',}
