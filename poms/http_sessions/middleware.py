d12724f354b72641c49d32f18c62da6384e486d5
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-06-24 09:46:02 +0300

    move from process_request to process_response

--- poms/http_sessions/middleware.py
+++ poms/http_sessions/middleware.py
@@ -2,10 +2,19 @@ from __future__ import unicode_literals
 
 
 class SessionMiddleware(object):
-    def process_request(self, request):
-        if hasattr(request, 'session'):
+    # def process_request(self, request):
+    #     if hasattr(request, 'session'):
+    #         if request.session.get('user_agent', None) != request.user_agent:
+    #             request.session['user_agent'] = request.user_agent
+    #
+    #         if request.session.get('user_ip', None) != request.user_ip:
+    #             request.session['user_ip'] = request.user_ip
+
+    def process_response(self, request, response):
+        if hasattr(request, 'session') and request.user.is_authenticated():
             if request.session.get('user_agent', None) != request.user_agent:
                 request.session['user_agent'] = request.user_agent
 
             if request.session.get('user_ip', None) != request.user_ip:
                 request.session['user_ip'] = request.user_ip
+        return response
