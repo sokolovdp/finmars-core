189e37866216c606fe8ed5db9e025406c914d698
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-07-01 16:20:09 +0300

    add kombu admin in DEBUG mode

--- poms/integrations/admin.py
+++ poms/integrations/admin.py
@@ -1,2 +1,33 @@
+from django.conf import settings
 from django.contrib import admin
+from kombu.transport.django.models import Queue, Message
 
+if settings.DEBUG and 'kombu.transport.django' in settings.INSTALLED_APPS:
+    class QueueAdmin(admin.ModelAdmin):
+        model = Queue
+        list_display = ('id', 'name')
+        list_display_links = ('id',)
+        search_fields = ('name',)
+
+
+    admin.site.register(Queue, QueueAdmin)
+
+
+    class MessageAdmin(admin.ModelAdmin):
+        model = Message
+        list_display = ('id', '_queue', 'visible', 'sent_at',)
+        list_display_links = ('id',)
+        # list_filter = ('queue',)
+        search_fields = ('queue__name',)
+        date_hierarchy = 'sent_at'
+        raw_id_fields = ('queue',)
+        list_select_related = ('queue',)
+
+        def _queue(self, instanse):
+            return instanse.queue.name
+
+        _queue.short_description = 'Queue'
+        _queue.admin_order_field = 'queue__name'
+
+
+    admin.site.register(Message, MessageAdmin)
