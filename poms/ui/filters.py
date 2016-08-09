434ba9cc18d77fcc276fddff93cf6114bbcf4b29
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-08-09 16:23:11 +0300

    fix ui ctype filter

--- poms/ui/filters.py
+++ poms/ui/filters.py
@@ -4,19 +4,19 @@ from rest_framework.filters import BaseFilterBackend
 
 class LayoutContentTypeFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        from poms.accounts.models import AccountType
-        from poms.accounts.models import Account
+        from poms.accounts.models import AccountType, Account
         from poms.currencies.models import Currency, CurrencyHistory
         from poms.instruments.models import InstrumentType, Instrument, PriceHistory, PricingPolicy
-        from poms.counterparties.models import Counterparty
-        from poms.counterparties.models import Responsible
-        from poms.strategies.models import Strategy1, Strategy2, Strategy3
+        from poms.counterparties.models import Counterparty, Responsible, CounterpartyGroup, ResponsibleGroup
+        from poms.strategies.models import Strategy1Group, Strategy1Subgroup, Strategy1, Strategy2Group, \
+            Strategy2Subgroup, Strategy2, Strategy3Group, Strategy3Subgroup, Strategy3
         from poms.portfolios.models import Portfolio
         from poms.transactions.models import TransactionType, Transaction
 
         models = [AccountType, Account, Currency, CurrencyHistory, InstrumentType, Instrument, PriceHistory,
-                  PricingPolicy, Counterparty, Responsible, Portfolio, TransactionType, Transaction,
-                  Strategy1, Strategy2, Strategy3, ]
+                  PricingPolicy, CounterpartyGroup, Counterparty, Responsible, ResponsibleGroup, Portfolio,
+                  TransactionType, Transaction, Strategy1Group, Strategy1Subgroup, Strategy1, Strategy2Group,
+                  Strategy2Subgroup, Strategy2, Strategy3Group, Strategy3Subgroup, Strategy3, ]
         ctypes = [ContentType.objects.get_for_model(model).pk for model in models]
         return queryset.filter(pk__in=ctypes).order_by('model')
 
