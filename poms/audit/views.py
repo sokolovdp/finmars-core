75f67b66760d6521df3e9b610873972b6ce78579
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-09-22 16:32:14 +0300

    add filter audit by master_user

--- poms/audit/views.py
+++ poms/audit/views.py
@@ -57,6 +57,9 @@ class ObjectHistory4ViewSet(AbstractReadOnlyModelViewSet):
         'master_user', 'member', 'actor_content_type', 'content_type', 'value_content_type', 'old_value_content_type',
         'actor_content_object', 'content_object', 'value_content_object', 'old_value_content_object')
     serializer_class = ObjectHistory4EntrySerializer
+    filter_backends = AbstractReadOnlyModelViewSet.filter_backends + [
+        OwnerByUserFilter,
+    ]
     permission_classes = AbstractReadOnlyModelViewSet.permission_classes + [
         SuperUserOnly,
     ]
