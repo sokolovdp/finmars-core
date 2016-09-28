75cdfcc022408240b254b385cc57c9e05ea6e5c0
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-09-28 10:31:28 +0300

    move prefetch to filter backend

--- poms/obj_perms/views.py
+++ poms/obj_perms/views.py
@@ -22,19 +22,19 @@ class AbstractWithObjectPermissionViewSet(AbstractModelViewSet):
     ]
     prefetch_permissions_for = []
 
-    def filter_queryset(self, queryset):
-        queryset = super(AbstractWithObjectPermissionViewSet, self).filter_queryset(queryset)
-        # queryset = ObjectPermissionBackend().filter_queryset(self.request, queryset, self)
-        queryset = obj_perms_prefetch(queryset, lookups_related=self.prefetch_permissions_for)
-        return queryset
+    # def filter_queryset(self, queryset):
+    #     queryset = super(AbstractWithObjectPermissionViewSet, self).filter_queryset(queryset)
+    #     if hasattr(self, 'prefetch_permissions_for'):
+    #         queryset = obj_perms_prefetch(queryset, lookups_related=self.prefetch_permissions_for)
+    #     return queryset
 
     # def get_serializer_class(self):
     #     # if self.action == 'objects_permissions':
     #     #     return self.bulk_objects_permissions_serializer_class
     #     return super(AbstractWithObjectPermissionViewSet, self).get_serializer_class()
 
-    def get_serializer(self, *args, **kwargs):
-        return super(AbstractWithObjectPermissionViewSet, self).get_serializer(*args, **kwargs)
+    # def get_serializer(self, *args, **kwargs):
+    #     return super(AbstractWithObjectPermissionViewSet, self).get_serializer(*args, **kwargs)
 
     # @list_route(methods=['POST'], url_path='objects-permissions')
     # def objects_permissions(self, request):
