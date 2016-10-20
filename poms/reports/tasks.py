9582dd0360d7a6d4511747776e190d2c5aa4d4e2
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-10-20 10:47:50 +0300

    pl report builder async

--- poms/reports/tasks.py
+++ poms/reports/tasks.py
@@ -5,6 +5,7 @@ import logging
 from celery import shared_task
 
 from poms.reports.backends.balance import BalanceReport2Builder
+from poms.reports.backends.pl import PLReport2Builder
 
 _l = logging.getLogger('poms.instruments')
 
@@ -34,54 +35,29 @@ def _balance_cache_key(master_user, cost_method, report_date, portfolios=None, a
 
 
 @shared_task(name='reports.balance_report')
-# def balance_report(master_user, cost_method, begin_date, end_date, portfolios=None, accounts=None, strategies1=None,
-#                    strategies2=None, strategies3=None, value_currency=None, use_portfolio=False,
-#                    use_account=False, use_strategy=False, custom_fields=None, **kwargs):
 def balance_report(instance):
+    _l.debug('balance_report: %s', instance)
     # _l.debug('balance_report: master_user=%s, cost_method=%s, begin_date=%s, end_date=%s, portfolios=%s, accounts=%s, '
-    #          'strategies1=%s, strategies2=%s, strategies3=%s, value_currency=%s, '
-    #          'use_portfolio=%s, use_account=%s, use_strategy=%s, custom_fields=%s',
-    #          master_user, cost_method, begin_date, end_date, portfolios, accounts,
-    #          strategies1, strategies2, strategies3, value_currency,
-    #          use_portfolio, use_account, use_strategy, custom_fields)
-    # _l.debug('balance_report: %s', pprint.pformat(instance, indent=2))
-    _l.debug('balance_report: master_user=%s, cost_method=%s, begin_date=%s, end_date=%s, portfolios=%s, accounts=%s, '
-             'strategies1=%s, strategies2=%s, strategies3=%s, value_currency=%s, use_portfolio=%s, use_account=%s, '
-             'use_strategy=%s, custom_fields=%s',
-             instance.master_user, instance.cost_method, instance.begin_date, instance.end_date, instance.portfolios,
-             instance.accounts, instance.strategies1, instance.strategies2, instance.strategies3,
-             instance.value_currency, instance.use_portfolio, instance.use_account, instance.use_strategy,
-             instance.custom_fields)
-    # serializer = BalanceReportSerializer(data=instance)
-    # serializer.is_valid(raise_exception=True)
-    # instance = serializer.save()
-
-    # master_user = MasterUser.objects.get(pk=master_user)
-    # cost_method = CostMethod.objects.get(pk=cost_method)
-    # report_date = date.fromordinal(report_date)
-
-    # portfolios = portfolios or []
-    # accounts = accounts or []
-    # strategies1 = strategies1 or []
-    # strategies2 = strategies2 or []
-    # strategies3 = strategies3 or []
-    # value_currency = value_currency or master_user.currency
-
-    # report = BalanceReport(
-    #     master_user=master_user,
-    #     begin_date=begin_date,
-    #     end_date=end_date,
-    #     use_portfolio=use_portfolio,
-    #     use_account=use_account,
-    #     use_strategy=use_strategy,
-    #     cost_method=cost_method,
-    # )
+    #          'strategies1=%s, strategies2=%s, strategies3=%s, value_currency=%s, use_portfolio=%s, use_account=%s, '
+    #          'use_strategy=%s, custom_fields=%s',
+    #          instance.master_user, instance.cost_method, instance.begin_date, instance.end_date, instance.portfolios,
+    #          instance.accounts, instance.strategies1, instance.strategies2, instance.strategies3,
+    #          instance.value_currency, instance.use_portfolio, instance.use_account, instance.use_strategy,
+    #          instance.custom_fields)
 
     builder = BalanceReport2Builder(instance=instance)
     instance = builder.build()
 
     _l.debug('finished')
+    return instance
 
-    # serializer = BalanceReportSerializer(instance=instance)
-    # return serializer.data
+
+@shared_task(name='reports.pl_report')
+def pl_report(instance):
+    _l.debug('pl_report: %s', instance)
+
+    builder = PLReport2Builder(instance=instance)
+    instance = builder.build()
+
+    _l.debug('finished')
     return instance
--- poms/reports/views.py
+++ poms/reports/views.py
@@ -6,14 +6,12 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 
 from poms.common.views import AbstractViewSet
-from poms.reports.backends.balance import BalanceReport2Builder
 from poms.reports.backends.cost import CostReport2Builder
-from poms.reports.backends.pl import PLReport2Builder
 from poms.reports.backends.simple_multipliers import SimpleMultipliersReport2Builder
 from poms.reports.backends.ytm import YTMReport2Builder
 from poms.reports.serializers import BalanceReportSerializer, PLReportSerializer, \
     CostReportSerializer, YTMReportSerializer, SimpleMultipliersReport2Serializer
-from poms.reports.tasks import balance_report
+from poms.reports.tasks import balance_report, pl_report
 
 
 class BaseReportViewSet(AbstractViewSet):
@@ -30,10 +28,8 @@ class BaseReportViewSet(AbstractViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class BalanceReport2ViewSet(BaseReportViewSet):
-    serializer_class = BalanceReportSerializer
-    report_builder_class = BalanceReport2Builder
-    async_task = balance_report
+class AsyncAbstractReportViewSet(AbstractViewSet):
+    async_task = None
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -59,9 +55,39 @@ class BalanceReport2ViewSet(BaseReportViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class PLReport2ViewSet(BaseReportViewSet):
+class BalanceReport2ViewSet(AsyncAbstractReportViewSet):
+    serializer_class = BalanceReportSerializer
+    # report_builder_class = BalanceReport2Builder
+    async_task = balance_report
+
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     instance = serializer.save()
+    #
+    #     task_id = instance.task_id
+    #     if task_id:
+    #         res = AsyncResult(task_id)
+    #         if res.ready():
+    #             instance = res.result
+    #         if instance.master_user.id != self.request.user.master_user.id:
+    #             raise PermissionDenied()
+    #         instance.task_id = task_id
+    #         instance.task_status = res.status
+    #         serializer = self.get_serializer(instance=instance, many=False)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     else:
+    #         res = self.async_task.apply_async(kwargs={'instance': instance})
+    #         instance.task_id = res.id
+    #         instance.task_status = res.status
+    #         serializer = self.get_serializer(instance=instance, many=False)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PLReport2ViewSet(AsyncAbstractReportViewSet):
     serializer_class = PLReportSerializer
-    report_builder_class = PLReport2Builder
+    # report_builder_class = PLReport2Builder
+    async_task = pl_report
 
 
 class CostReport2ViewSet(BaseReportViewSet):
