72d0f344048ebba5c384c3522969842070dbf8ad
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2016-10-21 10:42:05 +0300

    customize None value

--- poms/api/admin.py
+++ poms/api/admin.py
@@ -6,3 +6,4 @@ from django.utils.translation import ugettext_lazy
 admin.site.site_title = ugettext_lazy('Finmars site admin')
 admin.site.site_header = ugettext_lazy('Finmars administration')
 admin.site.index_title = ugettext_lazy('Finmars site administration')
+admin.site.empty_value_display = '<small>NULL</small>'
