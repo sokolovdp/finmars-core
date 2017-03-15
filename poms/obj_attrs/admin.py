c9e7b0e3009194faebab05f3774f8f8bc2a26eae
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2017-03-15 14:01:35 +0300

    fix admin

--- poms/obj_attrs/admin.py
+++ poms/obj_attrs/admin.py
@@ -92,9 +92,9 @@ class GenericClassifierAdmin(AbstractModelAdmin):
     list_display = ['id', 'master_user', 'attribute_type', 'content_type', 'tree_id', 'level', 'parent',
                     'name', ]
     list_select_related = ['attribute_type', 'attribute_type__master_user', 'attribute_type__content_type', 'parent']
-    search_fields = ['attribute_type_id']
-    raw_id_fields = ['attribute_type', 'parent']
+    search_fields = ['attribute_type__id', 'attribute_type__user_code', ]
     ordering = ['attribute_type', 'tree_id', 'level', ]
+    raw_id_fields = ['attribute_type', 'parent']
     save_as = True
 
     def master_user(self, obj):
@@ -118,10 +118,10 @@ class GenericAttributeAdmin(AbstractModelAdmin):
                     'content_object', 'value_string', 'value_float', 'value_date', 'classifier', ]
     list_select_related = ['content_type', 'attribute_type', 'attribute_type__master_user',
                            'attribute_type__content_type', 'classifier']
-    raw_id_fields = ['attribute_type', 'content_type', 'classifier']
     list_filter = ['content_type']
     ordering = ['attribute_type__master_user', 'attribute_type', ]
-    search_fields = ['object_id', 'attribute_type_id']
+    search_fields = ['attribute_type__id', 'attribute_type__user_code', ]
+    raw_id_fields = ['attribute_type', 'content_type', 'classifier']
     save_as = True
 
     def get_queryset(self, request):
