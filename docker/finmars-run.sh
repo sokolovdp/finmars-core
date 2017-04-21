09075833c279462828314ad2bea1b4a5211c25db
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2017-04-21 11:32:01 +0300

    run script

--- docker/finmars-run.sh
+++ docker/finmars-run.sh
@@ -4,6 +4,13 @@ echo "Finmars"
 source /var/app-venv/bin/activate
 cd /var/app/
 
-./manage.py
+
+function migrate {
+    ./manage.py migrate
+}
+
+function createsuperuser {
+    ./manage.py initstandalone
+}
 
 ./manage.py runserver 0.0.0.0:8080
