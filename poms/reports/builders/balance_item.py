f4ee122fd5d9edac2df3b5d88d443a3672218ef9
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2017-04-26 17:04:05 +0300

    use coorect fx for accrued

--- poms/reports/builders/balance_item.py
+++ poms/reports/builders/balance_item.py
@@ -1019,7 +1019,7 @@ class ReportItem(BaseReportItem):
             if self.instr:
                 self.instr_principal_res = self.pos_size * self.instr.price_multiplier * self.instr_price_cur_principal_price * self.instr_pricing_ccy_cur_fx
                 self.instr_principal_loc = self.pos_size * self.instr.price_multiplier * self.instr_price_cur_principal_price
-                self.instr_accrued_res = self.pos_size * self.instr.accrued_multiplier * self.instr_price_cur_accrued_price * self.instr_pricing_ccy_cur_fx
+                self.instr_accrued_res = self.pos_size * self.instr.accrued_multiplier * self.instr_price_cur_accrued_price * self.instr_accrued_ccy_cur_fx
                 self.instr_accrued_loc = self.pos_size * self.instr.accrued_multiplier * self.instr_price_cur_accrued_price
 
                 # _l.debug('> instr_accrual: instr=%s', self.instr.id)
