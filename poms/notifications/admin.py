6bf8c2da54d05934e30ca9c53312b74cb18db75f
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2017-04-28 18:28:58 +0300

    impove notifications

--- poms/notifications/admin.py
+++ poms/notifications/admin.py
@@ -9,10 +9,10 @@ from poms.notifications.models import Notification
 class NotificationAdmin(AbstractModelAdmin):
     model = Notification
     master_user_path = 'recipient_member__master_user'
-    list_display = ['id', 'recipient', 'recipient_member', 'create_date', '__str__',
+    list_display = ['id', 'master_user', 'recipient', 'recipient_member', 'create_date', '__str__',
                     'actor', 'verb', 'action_object', 'target']
     ordering = ['recipient_member', 'create_date']
-    list_select_related = ['recipient', 'recipient_member',]
+    list_select_related = ['recipient', 'recipient_member', ]
     raw_id_fields = ['recipient', 'recipient_member']
     date_hierarchy = 'create_date'
 
@@ -20,6 +20,11 @@ class NotificationAdmin(AbstractModelAdmin):
         queryset = super(NotificationAdmin, self).get_queryset(request)
         queryset.prefetch_related('actor', 'action_object', 'target')
         return queryset
+
+    def master_user(self, obj):
+        return getattr(obj.recipient_member, 'master_user', None)
+
+    master_user.admin_order_field = 'recipient_member__master_user'
 
 
 admin.site.register(Notification, NotificationAdmin)
