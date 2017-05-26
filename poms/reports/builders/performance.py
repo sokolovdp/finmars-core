4670398e03a286ccb2a19ac51e971bbe36becf14
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2017-05-26 16:41:24 +0300

    added transaction cloning

--- poms/reports/builders/performance.py
+++ poms/reports/builders/performance.py
@@ -33,6 +33,8 @@ class PerformanceReportBuilder(BaseReportBuilder):
             try:
                 self._load()
 
+                self._clone_transactions_if_need()
+
                 self.instance.items = [
                     PerformanceReportItem(
                         self.instance,
@@ -75,6 +77,120 @@ class PerformanceReportBuilder(BaseReportBuilder):
         self._transactions = list(qs)
 
         _l.debug('< _load: %s', len(self._transactions))
+
+    def _clone_transactions_if_need(self):
+        _l.debug('> clone_transactions_if_need')
+
+        res = []
+        for trn in self._transactions:
+            if trn.is_transfer:
+                trns = self._trn_transfer_clone(trn)
+                res.extend(trns)
+
+            elif trn.is_fx_transfer:
+                trns = self._trn_fx_transfer_clone(trn)
+                res.extend(trns)
+
+            else:
+                res.append(trn)
+
+        self._transactions = res
+
+        _l.debug('< clone_transactions_if_need: %s', len(self._transactions))
+
+    def _trn_transfer_clone(self, trn):
+        # split TRANSFER to sell/buy or buy/sell
+        if trn.position_size_with_sign >= 0:
+            t1_cls = self._trn_cls_sell
+            t2_cls = self._trn_cls_buy
+            t1_pos_sign = 1.0
+            t1_cash_sign = -1.0
+        else:
+            t1_cls = self._trn_cls_buy
+            t2_cls = self._trn_cls_sell
+            t1_pos_sign = -1.0
+            t1_cash_sign = 1.0
+
+        # t1
+        t1 = self._clone(trn)
+        t1.transaction_class = t1_cls
+
+        t1.position_size_with_sign = abs(trn.position_size_with_sign) * t1_pos_sign
+        t1.cash_consideration = abs(trn.cash_consideration) * t1_cash_sign
+        t1.principal_with_sign = abs(trn.principal_with_sign) * t1_cash_sign
+        t1.carry_with_sign = abs(trn.carry_with_sign) * t1_cash_sign
+        t1.overheads_with_sign = abs(trn.overheads_with_sign) * t1_cash_sign
+
+        t1.account_position = trn.account_cash
+        t1.account_cash = trn.account_cash
+        t1.strategy1_position = trn.strategy1_cash
+        t1.strategy1_cash = trn.strategy1_cash
+        t1.strategy2_position = trn.strategy2_cash
+        t1.strategy2_cash = trn.strategy2_cash
+        t1.strategy3_position = trn.strategy3_cash
+        t1.strategy3_cash = trn.strategy3_cash
+
+        # t2
+        t2 = self._clone(trn)
+        t2.transaction_class = t2_cls
+
+        t2.position_size_with_sign = -t1.position_size_with_sign
+        t2.cash_consideration = -t1.cash_consideration
+        t2.principal_with_sign = -t1.principal_with_sign
+        t2.carry_with_sign = -t1.carry_with_sign
+        t2.overheads_with_sign = -t1.overheads_with_sign
+
+        t2.account_position = trn.account_position
+        t2.account_cash = trn.account_position
+        t2.strategy1_position = trn.strategy1_position
+        t2.strategy1_cash = trn.strategy1_position
+        t2.strategy2_position = trn.strategy2_position
+        t2.strategy2_cash = trn.strategy2_position
+        t2.strategy3_position = trn.strategy3_position
+        t2.strategy3_cash = trn.strategy3_position
+
+        return t1, t2
+
+    def _trn_fx_transfer_clone(self, trn):
+        # t1
+        t1 = self._clone(trn)
+        t1.transaction_class = self._trn_cls_cash_out
+
+        t1.position_size_with_sign = -trn.position_size_with_sign
+        t1.cash_consideration = -trn.cash_consideration
+        t1.principal_with_sign = -trn.principal_with_sign
+        t1.carry_with_sign = -trn.carry_with_sign
+        t1.overheads_with_sign = -trn.overheads_with_sign
+
+        t1.account_position = trn.account_cash
+        t1.account_cash = trn.account_cash
+        t1.strategy1_position = trn.strategy1_cash
+        t1.strategy1_cash = trn.strategy1_cash
+        t1.strategy2_position = trn.strategy2_cash
+        t1.strategy2_cash = trn.strategy2_cash
+        t1.strategy3_position = trn.strategy3_cash
+        t1.strategy3_cash = trn.strategy3_cash
+
+        # t2
+        t2 = self._clone(trn)
+        t2.trn_cls = self._trn_cls_cash_in
+
+        t2.position_size_with_sign = -trn.position_size_with_sign
+        t2.cash_consideration = trn.cash_consideration
+        t2.principal_with_sign = trn.principal_with_sign
+        t2.carry_with_sign = trn.carry_with_sign
+        t2.overheads_with_sign = trn.overheads_with_sign
+
+        t2.account_position = trn.account_position
+        t2.account_cash = trn.account_position
+        t2.strategy1_position = trn.strategy1_position
+        t2.strategy1_cash = trn.strategy1_position
+        t2.strategy2_position = trn.strategy2_position
+        t2.strategy2_cash = trn.strategy2_position
+        t2.strategy3_position = trn.strategy3_position
+        t2.strategy3_cash = trn.strategy3_position
+
+        return t1, t2
 
     def _refresh_from_db(self):
         _l.info('> _refresh_from_db')
@@ -147,7 +263,6 @@ class PerformanceReportBuilder(BaseReportBuilder):
             items=self.instance.items,
             attrs=['strategy3']
         )
-
 
         self.instance.item_portfolios = self._refresh_portfolios(
             master_user=self.instance.master_user,
