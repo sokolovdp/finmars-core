097c4fec64bfd6fae3f5bc6df87360b8d8734b41
Alexander Ilyukhin <ailyukhin@vitaminsoft.com>
2017-06-15 13:42:28 +0300

    update calculation

--- poms/reports/tests_perf.py
+++ poms/reports/tests_perf.py
@@ -1,26 +1,13 @@
 import logging
-import uuid
-from datetime import date, timedelta
+from datetime import date
 
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.utils.functional import cached_property
 
-from poms.accounts.models import AccountType, Account
-from poms.currencies.models import Currency, CurrencyHistory
-from poms.instruments.models import Instrument, PriceHistory, PricingPolicy, CostMethod, InstrumentType, \
-    InstrumentClass, AccrualCalculationSchedule, AccrualCalculationModel, Periodicity, InstrumentFactorSchedule
-from poms.portfolios.models import Portfolio
+from poms.instruments.models import Instrument
 from poms.reports.builders.base_item import BaseReportItem
-from poms.reports.builders.cash_flow_projection import CashFlowProjectionReportBuilder
-from poms.reports.builders.cash_flow_projection_item import CashFlowProjectionReport
 from poms.reports.builders.performance import PerformanceReportBuilder
 from poms.reports.builders.performance_item import PerformanceReport
 from poms.reports.tests_cf import AbstractReportTestMixin
-from poms.strategies.models import Strategy1Group, Strategy1Subgroup, Strategy1, Strategy2Group, Strategy2Subgroup, \
-    Strategy2, Strategy3Subgroup, Strategy3Group, Strategy3
-from poms.transactions.models import Transaction, TransactionClass, TransactionType
-from poms.users.models import MasterUser, Member
 
 _l = logging.getLogger('poms.reports')
 
@@ -74,6 +61,62 @@ class PerfReportTestCase(AbstractReportTestMixin, TestCase):
         )
         self._instr_hist(i1, date(2020, 1, 31), 1, 1)
         self._instr_hist(i1, date(2020, 2, 29), 1, 1)
+
+        self._t_buy(
+            instr=i1, position=10,
+            stl_ccy=self.usd, principal=-10, carry=0, overheads=-1,
+            acc_pos=self.a1_1, acc_cash=self.a1_2,
+            acc_date=date(2020, 1, 10), cash_date=date(2020, 1, 10)
+        )
+
+        # self._t_buy(
+        #     instr=i1, position=10,
+        #     stl_ccy=self.usd, principal=-20, carry=0, overheads=-1,
+        #     acc_pos=self.a1_1, acc_cash=self.a1_2,
+        #     acc_date=date(2020, 2, 10), cash_date=date(2020, 2, 10)
+        # )
+
+        report = PerformanceReport(
+            master_user=self.m,
+            member=self.mm,
+            begin_date=date(2020, 1, 1),
+            end_date=date(2020, 12, 31),
+            report_currency=self.usd,
+            pricing_policy=self.pp,
+            periods='date_group(transaction.accounting_date,[[None,None,timedelta(months=1),["[","%Y-%m-%d","/","","%Y-%m-%d","]"]]], "Err")',
+        )
+        report_builder = PerformanceReportBuilder(report)
+        report_builder.build_performance()
+        self._dumps(report.items)
+
+    def test_perf2(self):
+        # settings.DEBUG = True
+
+        i1 = Instrument.objects.create(
+            master_user=self.m,
+            user_code='i1',
+            instrument_type=self.m.instrument_type,
+            pricing_currency=self.usd,
+            price_multiplier=1.0,
+            accrued_currency=self.usd,
+            accrued_multiplier=1.0,
+            maturity_date=date(2103, 1, 1),
+            maturity_price=1000,
+        )
+        i2 = Instrument.objects.create(
+            master_user=self.m,
+            user_code='i',
+            instrument_type=self.m.instrument_type,
+            pricing_currency=self.usd,
+            price_multiplier=1.0,
+            accrued_currency=self.usd,
+            accrued_multiplier=1.0,
+            maturity_date=date(2103, 1, 1),
+            maturity_price=1000,
+        )
+        for m in range(1,13):
+            self._instr_hist(i1, date(2020, m, 31), 1, 1)
+            self._instr_hist(i2, date(2020, m, 29), 1, 1)
 
         self._t_buy(
             instr=i1, position=10,
