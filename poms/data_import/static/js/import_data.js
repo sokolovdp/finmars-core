bbdc26e7cbd57e4136ef280950ad09d984ab39ab
mikhailborodin <mborodin87@gmail.com>
2018-02-15 14:24:53 +0300

    data_import app

--- poms/data_import/static/js/import_data.js
+++ poms/data_import/static/js/import_data.js
@@ -63,7 +63,7 @@ angular.module('portal', [
     }
     function put(model, id, data) {
       var defer = $q.defer();
-      $http.put('/api/v1/import/' + model + '/' + id + '/', data).
+      $http.put('/api/v1/import/' + model + '/' + id + '/', data, {headers: {'X-CSRFToken': getCookie('csrftoken')}}).
         success(function (data, status){
           defer.resolve({data: data, status: status});
         }).
@@ -72,6 +72,18 @@ angular.module('portal', [
         });
       return defer.promise
     }
+    function patch(model, id, data) {
+      var defer = $q.defer();
+      $http.patch('/api/v1/import/' + model + '/' + id + '/', data, {headers: {'X-CSRFToken': getCookie('csrftoken')}}).
+        success(function (data, status){
+          defer.resolve({data: data, status: status});
+        }).
+        error(function (msg, status) {
+          defer.resolve({msg: msg, status: status});
+        });
+      return defer.promise
+    }
+
     function del(model, id) {
       var defer = $q.defer();
       $http.delete('/api/v1/import/' + model + '/' + id + '/', {headers: {'X-CSRFToken': getCookie('csrftoken')}}).
@@ -87,6 +99,7 @@ angular.module('portal', [
       get: get,
       post: post,
       put: put,
+      patch: patch,
       delete: del,
       getCookie: getCookie
     }
@@ -144,8 +157,11 @@ angular.module('portal', [
             clickOutsideToClose:true,
             fullscreen: $scope.customFullscreen,
             locals : {
+                data: null,
+                vm: null,
                 schema: $scope.selectedItem,
-                field_list: $scope.field_list
+                field_list: $scope.field_list,
+                item: null
             }
           })
           .then(function(answer) {
@@ -163,8 +179,11 @@ angular.module('portal', [
           clickOutsideToClose:true,
           fullscreen: $scope.customFullscreen,
           locals : {
+            data: null,
+            vm: null,
             schema: null,
-            field_list: null
+            field_list: null,
+            item: null
           }
         })
         .then(function(answer) {
@@ -174,20 +193,19 @@ angular.module('portal', [
         });
       }
     };
-    function DialogController($scope, $mdDialog, api, schema, field_list) {
+    function DialogController($scope, $mdDialog, api, schema, field_list, item, data, vm) {
+      $scope.data = data;
+      $scope.vm = vm;
       $scope.schema = schema;
+      $scope.mapping = {'value': null, 'name': null};
       $scope.field_list = field_list;
-      api.get('schema_models').then(function (resp) {
-        $scope.models = resp.data.results;
-      });
-      $scope.$watch('schema.model', function(newVal, oldVal){
-        // api.get('content_type/' + newVal + '/fields').then(function (resp) {
-        //   $scope.matching_list = resp.data.results;
-        // });
-        api.get('schema_matching', {schema_id: $scope.schema.id}).then(function (resp) {
-          $scope.matching_list = resp.data;
-        })
-      });
+      if (schema) {
+        $scope.$watch('schema.model', function(newVal, oldVal){
+          api.get('schema_matching', {schema_id: $scope.schema.id}).then(function (resp) {
+            $scope.matching_list = resp.data;
+          })
+        });
+      }
       $scope.copyField = function(){
         var last_num = 0;
         if ($scope.field_list.length > 0) {
@@ -197,10 +215,16 @@ angular.module('portal', [
       };
       $scope.removeField = function(item, index){
         $scope.field_list.splice(index, 1);
-        api.delete('schema_fields', item.id).then(function(resp){
-
-        })
+        api.delete('schema_fields', item.id).then(function(resp){})
       };
+      $scope.$watch('mapping.model', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+          var id = newVal.split(',')[0];
+          api.get('content_type/' + id + '/fields').then(function (resp){
+            $scope.data.fields = resp.data.results;
+          })
+        }
+      });
       $scope.saveSchema = function(){
         api.post('schema_fields', {'field_list': $scope.field_list, 'matching_list': $scope.matching_list} ).then(function(resp){
           $scope.hide()
@@ -209,20 +233,246 @@ angular.module('portal', [
       $scope.hide = function() {
         $mdDialog.hide();
       };
-
       $scope.cancel = function() {
         $mdDialog.cancel();
       };
-
       $scope.answer = function(answer) {
         $mdDialog.hide(answer);
       };
+      $scope.openExpressionDialog = function(ev, item){
+        var vm = $scope;
+        vm.readyStatus = {
+            expression: false
+        };
+
+        vm.item = item;
+        var getFunctionsHelp = function() {
+          return [
+            {
+              "name": "To string",
+              "description": "<p>Any value to string</p>",
+              "func": "str(a)"
+            },
+            {
+              "name": "Contains",
+              "description": "<p>String a contains or not in string b</p>",
+              "func": "contains(a, b)"
+            },
+            {
+              "name": "To integer",
+              "description": "<p>Convert string to integer</p>",
+              "func": "int(a)"
+            },
+            {
+              "name": "To float",
+              "description": "<p>Convert string to number</p>",
+              "func": "float(a)"
+            },
+            {
+              "name": "Round",
+              "description": "<p>Match round float</p>",
+              "func": "round(number)"
+            },
+            {
+              "name": "Trunc",
+              "description": "<p>Match truncate float</p>",
+              "func": "trunc(a)"
+            },
+            {
+              "name": "Is close",
+              "description": "<p>Compare to float numbers to equality</p>",
+              "func": "isclose(a, b)"
+            },
+            {
+              "name": "iff",
+              "description": "<p>Return a if x is True else v2</p>",
+              "func": "iff(expr, a, b)"
+            },
+            {
+              "name": "Now",
+              "description": "<p>Current date</p>",
+              "func": "now()"
+            },
+            {
+              "name": "Date",
+              "description": "<p>Create date object</p>",
+              "func": "date(year, month=1, day=1)"
+            },
+            {
+              "name": "Days",
+              "description": "<p>Create timedelta object for operations with dates <br/> now() - days(10)<br/> now() + days(10)</p>",
+              "func": "days(a)"
+            },
+            {
+              "name": "Weeks",
+              "description": "<p>Create timedelta object for operations with dates <br/> now() - weeks(10)<br/> now() + weeks(10)</p>",
+              "func": "weeks(a)"
+            },
+            {
+              "name": "Months",
+              "description": "<p>Create timedelta object for operations with dates <br/> now() - months(10)<br/> now() + months(10)</p>",
+              "func": "months(a)"
+            },
+            {
+              "name": "Timedelta",
+              "description": "<p>General timedelta creation</p><p>years, months, weeks, days:<br/> Relative information, may be negative (argument is plural); adding or subtracting a relativedelta with relative information performs the corresponding aritmetic operation on the original datetime value with the information in the relativedelta.</p><p>leapdays:<br/> Will add given days to the date found, if year is a leap year, and the date found is post 28 of february.</p>",
+              "func": "timedelta(years=0, months=0, days=0, leapdays=0, weeks=0)"
+            },
+            {
+              "name": "Add days",
+              "description": "<p>Same as date + days(x)</p>",
+              "func": "add_days(date, days)"
+            },
+            {
+              "name": "Add weeks",
+              "description": "<p>Same as d + days(x * 7)</p>",
+              "func": "add_weeks(date, days)"
+            },
+            {
+              "name": "Add workdays",
+              "description": "<p>Add 'x' work days to d</p>",
+              "func": "add_workdays(date, workdays)"
+            },
+            {
+              "name": "Format date",
+              "description": "<p>format date (default format is '%Y-%m-%d')</p>",
+              "func": "format_date(date, format='%Y-%m-%d')"
+            },
+            {
+              "name": "Parse date",
+              "description": "<p>parse date from string (default format is '%Y-%m-%d')</p>",
+              "func": "parse_date(date_string, format='%Y-%m-%d')"
+            },
+            {
+              "name": "Format number",
+              "description": "<p>format float number<br/> <br/> decimal_sep:<br/> Decimal separator symbol (for example '.')<br/> decimal_pos:<br/> Number of decimal positions<br/> grouping:<br/> Number of digits in every group limited by thousand separator<br/> thousand_sep:<br/> Thousand separator symbol (for example ',')<br/> use_grouping:<br/> use thousand separator</p>",
+              "func": "format_number(number, decimal_sep='.', decimal_pos=None, grouping=3, thousand_sep='', use_grouping=False)"
+            },
+            {
+              "name": "Parse number",
+              "description": "<p>same as float(a)</p>",
+              "func": "parse_number(a)"
+            },
+            {
+              "name": "Simple price",
+              "description": "<p>calculate price on date using 2 point (date1, value1) and (date2, value2)</p>",
+              "func": "simple_price(date, date1, value1, date2, value2)"
+            }
+          ]
+        };
+        vm.expressions = getFunctionsHelp();
+        vm.readyStatus.expression = true;
+        vm.selectedHelpItem = vm.expressions[0];
+        vm.selectHelpItem = function(item) {
+            vm.expressions.forEach(function(expr) {
+                expr.isSelected = false;
+            });
+
+            item.isSelected = true;
+
+            vm.selectedHelpItem = item;
+        };
+
+        vm.appendFunction = function(item) {
+            var val = $('#editorExpressionInput')[0].value;
+            var cursorPosition = val.slice(0, ($('#editorExpressionInput')[0].selectionStart + '')).length;
+
+            if (cursorPosition == 0) {
+                vm.item.expression = vm.item.expression + item.func;
+            } else {
+                vm.item.expression = vm.item.expression.slice(0, cursorPosition) + item.func + vm.item.expression.slice(cursorPosition);
+
+            }
+        };
+
+        vm.cancel = function() {
+            $mdDialog.cancel();
+        };
+
+        vm.agree = function() {
+            api.patch('schema_matching', item.id, {'expression': item.expression});
+            $mdDialog.hide({
+                status: 'agree',
+                data: {
+                    item: vm.item
+                }
+            });
+        };
+
+        vm.openHelp = function($event) {
+            $mdDialog.show({
+                controller: 'HelpDialogController as vm',
+                templateUrl: 'views/dialogs/help-dialog-view.html',
+                targetEvent: $event,
+                locals: {
+                    data: {}
+                },
+                preserveScope: true,
+                autoWrap: true,
+                skipHide: true
+            })
+        };
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/static/js/expression-editor-dialog-view.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: $scope.customFullscreen,
+            locals : {
+              data: {models: $scope.models},
+              vm: vm,
+              item: item,
+              schema: null,
+              field_list: null
+            }
+          })
+          .then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+          }, function() {
+            $scope.status = 'You cancelled the dialog.';
+          });
+      };
+      $scope.openMapping = function (ev, item) {
+        api.get('content_type').then(function(resp){
+          $scope.models = resp.data.results;
+          $mdDialog.show({
+            controller: DialogController,
+            templateUrl: '/static/js/entity-type-mapping-dialog-view.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: $scope.customFullscreen,
+            locals : {
+              data: {models: $scope.models},
+              item: item,
+              schema: null,
+              vm: null,
+              field_list: null
+            }
+          })
+          .then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+          }, function() {
+            $scope.status = 'You cancelled the dialog.';
+          });
+        });
+      };
+      $scope.saveMapping = function (mapping) {
+        var model = mapping.model.split(',')[1];
+        api.patch('schema_matching', item.id, {model_field: model + ':' + mapping.field});
+        $mdDialog.hide();
+      };
+      
     }
 
   })
   .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('docs-dark', 'default')
-      .primaryPalette('yellow')
-      .dark();
-
-  });
+    $mdThemingProvider.theme('docs-dark', 'default').primaryPalette('yellow').dark();
+  })
+  .filter('trustAsHtml', ['$sce', function($sce) {return function (val){
+      if (val) {
+        return $sce.trustAsHtml(val.toString());
+      }
+    }
+  }]);
--- poms/data_import/views.py
+++ poms/data_import/views.py
@@ -1,15 +1,17 @@
 import io
 import csv
+import re
 from dateutil.parser import parse
 from django.db import IntegrityError
 from django.db.models.base import ModelBase
 from django.contrib.contenttypes.models import ContentType
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route, list_route
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
 from rest_framework.status import HTTP_201_CREATED, HTTP_500_INTERNAL_SERVER_ERROR
+from poms.common.formula import safe_eval, InvalidExpression
 from poms.obj_attrs.models import GenericAttributeType, GenericAttribute
 from poms.users.models import Member
 from .serializers import DataImportSerializer, DataImportSchemaSerializer, DataImportSchemaFieldsSerializer, \
@@ -44,22 +46,48 @@ class DataImportViewSet(viewsets.ModelViewSet):
                         pass
                 for matching_field in matchings_fields:
                     try:
-                        data[matching_field.model_field] = raw_data.get(matching_field.expression)
+                        if raw_data.get(matching_field.expression):
+                            data[matching_field.model_field] = raw_data.get(matching_field.expression)
+                        else:
+                            try:
+                                dict_row = {}
+                                for field in fields:
+                                    if isinstance(values[field.num], str):
+                                        dict_row[field.source] = '"' + values[field.num] + '"'
+                                    else:
+                                        dict_row[field.source] = values[field.num]
+                                expr = matching_field.expression.format(**dict_row)
+                                data[matching_field.model_field] = safe_eval(expr)
+                            except InvalidExpression:
+                                pass
                     except IndexError:
                         pass
                 if data:
                     try:
                         master_user_id = Member.objects.get(user=self.request.user).master_user.id
-                        accepted_data = {key: data.get(key) for key in PUBLIC_FIELDS[schema.model.model]}
-                        for k, v in accepted_data.items():
+                        accepted_data = {}
+                        additional_data = {}
+                        for key in PUBLIC_FIELDS[schema.model.model]:
+                            accepted_data[key] = data.get(key)
+                        for k, v in accepted_data.copy().items():
                             if isinstance(k, ModelBase):
                                 del accepted_data[k]
-                                rel_obj = k(**{'user_code': data[k._meta.model_name], 'master_user_id': master_user_id})
-                                rel_obj.save()
+                                try:
+                                    r = re.compile(k._meta.model_name)
+                                    data_key = list(filter(r.match, data.keys()))
+                                    if len(data_key) > 0:
+                                        rel_obj = k(**{data_key.split(':')[1]: data[data_key[0]], 'master_user_id': master_user_id})
+                                        rel_obj.save()
+                                except KeyError:
+                                    continue
                             else:
                                 accepted_data[k] = v
-                        additional_keys = [item for item in data.keys() if item not in accepted_data.keys()]
-                        additional_data = {k: data[k] for k in additional_keys}
+                        additional_keys = []
+                        for item in data.keys():
+                            if item not in accepted_data.keys() and len(item.split(':')) == 1:
+                                additional_keys.append(item)
+                        for k in additional_keys:
+                            accepted_data[k] = data[k]
                         accepted_data['master_user_id'] = master_user_id
                         accepted_data['attrs'] = additional_data
                         o = schema.model.model_class()(**accepted_data)
@@ -67,15 +95,16 @@ class DataImportViewSet(viewsets.ModelViewSet):
                         for additional_key in additional_keys:
                             attr_type = GenericAttributeType.objects.filter(user_code=additional_key).first()
                             attribute = GenericAttribute(content_object=o, attribute_type=attr_type)
-                            if attr_type.value_type == 40:
-                                attribute.value_date = parse(additional_data[additional_key])
-                            elif attr_type.value_type == 10:
-                                attribute.value_string = additional_data[additional_key]
-                            elif attr_type.value_type == 20:
-                                attribute.value_float = additional_data[additional_key]
-                            else:
-                                pass
-                            attribute.save()
+                            if attr_type:
+                                if attr_type.value_type == 40:
+                                    attribute.value_date = parse(additional_data[additional_key])
+                                elif attr_type.value_type == 10:
+                                    attribute.value_string = additional_data[additional_key]
+                                elif attr_type.value_type == 20:
+                                    attribute.value_float = additional_data[additional_key]
+                                else:
+                                    pass
+                                attribute.save()
                     except IntegrityError:
                         if int(request.data.get('error_handling')[0]):
                             continue
@@ -145,17 +174,18 @@ class DataImportSchemaMatchingViewSet(viewsets.ModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
         schema = DataImportSchema.objects.get(pk=self.request.query_params.get('schema_id'))
-        base_fields = list(map(lambda f: {'model_field': f, 'expression': ''} if isinstance(f, str) else {'model_field': '%s:user_code' % f._meta.model_name, 'expression': '', 'related': True}, PUBLIC_FIELDS[schema.model.name]))
+        base_fields = list(map(lambda f: {'model_field': f if isinstance(f, str) else f._meta.model_name, 'expression': '', 'related': False if isinstance(f, str) else True}, PUBLIC_FIELDS[schema.model.name]))
         master_user = Member.objects.get(user=self.request.user).master_user
         all_attr_fields = GenericAttributeType.objects.filter(master_user=master_user, content_type=schema.model)
         additional_fields = []
         for attr_fields in all_attr_fields:
-            additional_fields.append({'model_field': attr_fields.name, 'expression': ''})
+            additional_fields.append({'model_field': attr_fields.name, 'expression': '', 'related': False})
         all_fields = base_fields + additional_fields
         for f in serializer.data:
             if f['expression']:
                 for i, item in enumerate(all_fields):
-                    if f['model_field'] == item['model_field']:
+                    if f['model_field'].split(':')[0] == item['model_field']:
+                        f['related'] = item['related']
                         all_fields[i] = f
         return Response(all_fields)
 
@@ -171,12 +201,5 @@ class ContentTypeViewSet(viewsets.ModelViewSet):
     @detail_route(methods=['get'])
     def fields(self, request, pk=None):
         obj = self.get_object()
-        base_fields = list(map(lambda f: {'model_field': f, 'expression': ''} if isinstance(f, str) else {
-            'model_field': '%s (Related)' % f._meta.model_name, 'expression': '', 'related': True},
-                               PUBLIC_FIELDS[obj.model]))
-        master_user = Member.objects.get(user=self.request.user).master_user
-        all_attr_fields = GenericAttributeType.objects.filter(master_user=master_user, content_type=obj)
-        additional_fields = []
-        for attr_fields in all_attr_fields:
-            additional_fields.append({'model_field': attr_fields.name, 'expression': ''})
-        return Response({'results': base_fields + additional_fields})
+        model = obj.model_class()
+        return Response({'results': [f.name for f in model._meta.fields]})
