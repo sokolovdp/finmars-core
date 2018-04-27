cf49d8b4a2e446f4d950f00cd361ce4354fd50f2
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-04-27 14:34:25 +0300

    fix error result

--- poms/csv_import/views.py
+++ poms/csv_import/views.py
@@ -61,61 +61,61 @@ class CsvDataImportViewSet(viewsets.ModelViewSet):
 
             if row_number != 0:
 
-                try:
+                # try:
 
-                    print('scheme.content_type.model %s' % scheme.content_type.model)
+                print('scheme.content_type.model %s' % scheme.content_type.model)
 
-                    csv_row_dict = self.get_row_data(row, csv_fields)
+                csv_row_dict = self.get_row_data(row, csv_fields)
 
-                    instance = {}
-                    instance['master_user'] = Member.objects.get(user=request.user).master_user
-                    instance['attributes'] = []
+                instance = {}
+                instance['master_user'] = Member.objects.get(user=request.user).master_user
+                instance['attributes'] = []
 
-                    for entity_field in entity_fields:
+                for entity_field in entity_fields:
 
-                        key = entity_field.system_property_key
+                    key = entity_field.system_property_key
 
-                        if key is not None:
+                    if key is not None:
 
-                            if entity_field.expression != '':
+                        if entity_field.expression != '':
 
-                                if key == 'counterparties':
-                                    instance[key] = CounterpartyMapping.objects.get(
-                                        value=csv_row_dict[entity_field.expression]).content_object
-                                elif key == 'responsibles':
-                                    instance[key] = ResponsibleMapping.objects.get(
-                                        value=csv_row_dict[entity_field.expression]).content_object
-                                elif key == 'accounts':
-                                    instance[key] = AccountMapping.objects.get(
-                                        value=csv_row_dict[entity_field.expression]).content_object
-                                elif key == 'portfolios':
-                                    instance[key] = PortfolioMapping.objects.get(
-                                        value=csv_row_dict[entity_field.expression]).content_object
-                                else:
-                                    instance[key] = safe_eval(entity_field.expression,
-                                                              names=csv_row_dict)
-                        else:
+                            if key == 'counterparties':
+                                instance[key] = CounterpartyMapping.objects.get(
+                                    value=csv_row_dict[entity_field.expression]).content_object
+                            elif key == 'responsibles':
+                                instance[key] = ResponsibleMapping.objects.get(
+                                    value=csv_row_dict[entity_field.expression]).content_object
+                            elif key == 'accounts':
+                                instance[key] = AccountMapping.objects.get(
+                                    value=csv_row_dict[entity_field.expression]).content_object
+                            elif key == 'portfolios':
+                                instance[key] = PortfolioMapping.objects.get(
+                                    value=csv_row_dict[entity_field.expression]).content_object
+                            else:
+                                instance[key] = safe_eval(entity_field.expression,
+                                                          names=csv_row_dict)
+                    else:
 
-                            executed_attr = {}
-                            executed_attr['dynamic_attribute_id'] = entity_field.dynamic_attribute_id
-                            executed_attr['executed_expression'] = safe_eval(entity_field.expression,
-                                                                             names=csv_row_dict)
+                        executed_attr = {}
+                        executed_attr['dynamic_attribute_id'] = entity_field.dynamic_attribute_id
+                        executed_attr['executed_expression'] = safe_eval(entity_field.expression,
+                                                                         names=csv_row_dict)
 
-                            instance['attributes'].append(executed_attr)
+                        instance['attributes'].append(executed_attr)
 
-                    if instance['user_code'] == '':
-                        instance['user_code'] = instance['name']
+                if instance['user_code'] == '':
+                    instance['user_code'] = instance['name']
 
-                    results.append(instance)
+                results.append(instance)
 
-                except:
-
-                    instance['master_user'] = None
-
-                    errors.append({"line": row_number, 'instance': instance})
-
-                    if error_handler == 'break':
-                        break
+                # except:
+                #
+                #     instance['master_user'] = None
+                #
+                #     errors.append({"line": row_number, 'instance': instance})
+                #
+                #     if error_handler == 'break':
+                #         break
 
             row_number = row_number + 1
 
@@ -145,76 +145,76 @@ class CsvDataImportViewSet(viewsets.ModelViewSet):
 
         for row_number, result in results:
 
-            try:
+            # try:
 
-                result_without_many_to_many = {}
+            result_without_many_to_many = {}
 
-                many_to_many_fields = ['counterparties', 'responsibles', 'accounts', 'portfolios']
+            many_to_many_fields = ['counterparties', 'responsibles', 'accounts', 'portfolios']
 
-                for key, value in result.items():
+            for key, value in result.items():
 
-                    print('key %s' % key)
+                print('key %s' % key)
 
-                    if key != 'attributes':
+                if key != 'attributes':
 
-                        if key not in many_to_many_fields:
-                            result_without_many_to_many[key] = value
+                    if key not in many_to_many_fields:
+                        result_without_many_to_many[key] = value
 
-                print('result_without_many_to_many %s ' % result_without_many_to_many)
+            print('result_without_many_to_many %s ' % result_without_many_to_many)
 
-                instance = Model.objects.create(**result_without_many_to_many)
+            instance = Model.objects.create(**result_without_many_to_many)
 
-                print('saved result %s' % instance)
+            print('saved result %s' % instance)
 
-                for key, value in result.items():
+            for key, value in result.items():
 
-                    if key == 'counterparties':
-                        getattr(instance, key, False).add(result[key])
-                    elif key == 'responsibles':
-                        getattr(instance, key, False).add(result[key])
-                    elif key == 'accounts':
-                        getattr(instance, key, False).add(result[key])
-                    elif key == 'portfolios':
-                        getattr(instance, key, False).add(result[key])
+                if key == 'counterparties':
+                    getattr(instance, key, False).add(result[key])
+                elif key == 'responsibles':
+                    getattr(instance, key, False).add(result[key])
+                elif key == 'accounts':
+                    getattr(instance, key, False).add(result[key])
+                elif key == 'portfolios':
+                    getattr(instance, key, False).add(result[key])
 
-                print('result %s' % result['attributes'])
+            print('result %s' % result['attributes'])
 
-                for result_attr in result['attributes']:
+            for result_attr in result['attributes']:
 
-                    print('attr', result_attr)
+                print('attr', result_attr)
 
-                    attr_type = GenericAttributeType.objects.get(pk=result_attr['dynamic_attribute_id'])
+                attr_type = GenericAttributeType.objects.get(pk=result_attr['dynamic_attribute_id'])
 
-                    print('attr_type %s' % attr_type)
+                print('attr_type %s' % attr_type)
 
-                    print(repr(instance))
+                print(repr(instance))
 
-                    attribute = GenericAttribute(content_object=instance, attribute_type=attr_type)
+                attribute = GenericAttribute(content_object=instance, attribute_type=attr_type)
 
-                    if attr_type:
-                        if attr_type.value_type == 40:
-                            attribute.value_date = result_attr['executed_expression']
-                        elif attr_type.value_type == 10:
-                            attribute.value_string = result_attr['executed_expression']
-                        elif attr_type.value_type == 20:
-                            attribute.value_float = result_attr['executed_expression']
-                        else:
-                            pass
+                if attr_type:
+                    if attr_type.value_type == 40:
+                        attribute.value_date = result_attr['executed_expression']
+                    elif attr_type.value_type == 10:
+                        attribute.value_string = result_attr['executed_expression']
+                    elif attr_type.value_type == 20:
+                        attribute.value_float = result_attr['executed_expression']
+                    else:
+                        pass
 
-                    print('attribute %s' % attribute)
+                print('attribute %s' % attribute)
 
-                    attribute.save()
+                attribute.save()
 
-                instance.save()
+            instance.save()
 
-            except:
-
-                result['master_user'] = None
-
-                errors.append({"line": row_number + 1, 'instance': result})  # we skipped first row in csv, so +1
-
-                if error_handler == 'break':
-                    break
+            # except:
+            #
+            #     result['master_user'] = None
+            #
+            #     errors.append({"line": row_number + 1, 'instance': result})  # we skipped first row in csv, so +1
+            #
+            #     if error_handler == 'break':
+            #         break
 
         if len(errors):
             return Response(errors, status=status.HTTP_400_BAD_REQUEST)
