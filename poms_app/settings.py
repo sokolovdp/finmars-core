f2fc1222c8ee05447e8a4c68d54e6afcf619c836
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-05-17 18:06:27 +0300

    test

--- poms_app/settings.py
+++ poms_app/settings.py
@@ -226,7 +226,12 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 USE_ETAGS = True
+
+print('Here? DEBUG %s' % DEBUG)
+
 if not DEBUG:
+
+    print('Here?')
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
