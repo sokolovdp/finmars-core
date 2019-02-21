990ee4c39f767dcc08cad9d7a12a3904e42ded66
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-02-21 16:30:17 +0300

    fix shell

--- local-development/run_migrate.sh
+++ local-development/run_migrate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 . ../venv/bin/activate
-RDS_DB_NAME=finmars_dev RDS_USERNAME=postgres RDS_PASSWORD=postgres RDS_HOSTNAME=localhost RDS_PORT=5434 python manage.py migrate transactions 0024
+#RDS_DB_NAME=finmars_dev RDS_USERNAME=postgres RDS_PASSWORD=postgres RDS_HOSTNAME=localhost RDS_PORT=5434 python manage.py migrate transactions 0026
+RDS_DB_NAME=finmars_dev RDS_USERNAME=postgres RDS_PASSWORD=postgres RDS_HOSTNAME=localhost RDS_PORT=5434 python manage.py migrate
