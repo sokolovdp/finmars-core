f1c3413505af078290178ea9b6e22a0be60d70af
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-03-19 11:53:03 +0300

    fix reports

--- poms/reports/views.py
+++ poms/reports/views.py
@@ -11,6 +11,7 @@ from poms.reports.builders.balance_pl import ReportBuilder
 from poms.reports.builders.balance_serializers import BalanceReportSerializer, PLReportSerializer
 from poms.reports.builders.cash_flow_projection_serializers import CashFlowProjectionReportSerializer
 from poms.reports.builders.performance_serializers import PerformanceReportSerializer
+from poms.reports.builders.transaction import TransactionReportBuilder
 from poms.reports.builders.transaction_serializers import TransactionReportSerializer
 from poms.reports.models import CustomField
 from poms.reports.serializers import CustomFieldSerializer
@@ -80,26 +81,79 @@ class BalanceReportViewSet(AbstractViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+# class PLReportViewSet(AbstractAsyncViewSet):
+#     serializer_class = PLReportSerializer
+#     celery_task = pl_report
 
-class BalanceReportSyncViewSet(AbstractSyncViewSet):
-    serializer_class = BalanceReportSerializer
-    task = balance_report
 
-
-class PLReportViewSet(AbstractAsyncViewSet):
+class PLReportViewSet(AbstractViewSet):
     serializer_class = PLReportSerializer
-    celery_task = pl_report
+
+    def create(self, request, *args, **kwargs):
+        print('AbstractSyncViewSet create')
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+
+        builder = ReportBuilder(instance=instance)
+        instance = builder.build_pl()
+
+        instance.task_id = 1
+        instance.task_status = "SUCCESS"
 
 
-class TransactionReportViewSet(AbstractAsyncViewSet):
+        serialize_report_st = time.perf_counter()
+
+        serializer = self.get_serializer(instance=instance, many=False)
+
+        print('serialize report done: %s' % (time.perf_counter() - serialize_report_st))
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+#
+# class TransactionReportViewSet(AbstractAsyncViewSet):
+#     serializer_class = TransactionReportSerializer
+#     celery_task = transaction_report
+#
+#     def get_serializer_context(self):
+#         context = super(TransactionReportViewSet, self).get_serializer_context()
+#         context['attributes_hide_objects'] = True
+#         context['custom_fields_hide_objects'] = True
+#         return context
+
+
+class TransactionReportViewSet(AbstractViewSet):
     serializer_class = TransactionReportSerializer
-    celery_task = transaction_report
 
     def get_serializer_context(self):
         context = super(TransactionReportViewSet, self).get_serializer_context()
         context['attributes_hide_objects'] = True
         context['custom_fields_hide_objects'] = True
         return context
+
+    def create(self, request, *args, **kwargs):
+        print('AbstractSyncViewSet create')
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+
+        builder = TransactionReportBuilder(instance)
+        builder.build()
+        instance = builder.instance
+
+        instance.task_id = 1
+        instance.task_status = "SUCCESS"
+
+
+        serialize_report_st = time.perf_counter()
+
+        serializer = self.get_serializer(instance=instance, many=False)
+
+        print('serialize report done: %s' % (time.perf_counter() - serialize_report_st))
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 
 class CashFlowProjectionReportViewSet(AbstractAsyncViewSet):
