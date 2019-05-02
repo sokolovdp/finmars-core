c6a16fb24ae975b468f420b526a78809876a0ffb
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-05-02 17:10:05 +0300

    pl first date?

--- poms/reports/builders/balance_pl.py
+++ poms/reports/builders/balance_pl.py
@@ -396,6 +396,9 @@ class ReportBuilder(BaseReportBuilder):
         filters = Q(**{'%s__lte' % self.instance.date_field: self.instance.report_date})
         # filters = Q(**{'%s__lt' % self.instance.date_field: self.instance.report_date})
 
+        if self.instance.pl_first_date:
+            filters = Q(**{'%s__gt' % self.instance.date_field: self.instance.pl_first_date})
+
         if self.instance.instruments:
             # kw_filters['instrument__in'] = self.instance.instruments
             filters &= Q(instrument__in=self.instance.instruments)
