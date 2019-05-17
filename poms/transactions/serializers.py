6282a6ca0fa55d07885fa7ab3b7a8d8237d77aa1
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-05-17 12:51:03 +0300

    sync

--- poms/transactions/serializers.py
+++ poms/transactions/serializers.py
@@ -1332,6 +1332,15 @@ class TransactionTypeViewSerializer(ModelWithObjectPermissionSerializer):
             'id', 'group', 'user_code', 'name', 'short_name', 'public_name', 'notes',
             'is_valid_for_all_portfolios', 'is_valid_for_all_instruments', 'is_deleted',
             'group_object',
+
+            'user_text_1', 'user_text_2', 'user_text_3', 'user_text_4', 'user_text_5',
+            'user_text_6', 'user_text_7', 'user_text_8', 'user_text_9', 'user_text_10',
+
+            'user_number_1', 'user_number_2', 'user_number_3', 'user_number_4', 'user_number_5',
+            'user_number_6', 'user_number_7', 'user_number_8', 'user_number_9', 'user_number_10',
+
+            'user_date_1', 'user_date_2', 'user_date_3', 'user_date_4', 'user_date_5'
+            
         ]
 
 
