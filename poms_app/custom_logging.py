b3139a94bb5550411a220931e9e4b048e054ff35
Sergei Zhitenev <s.zhitenev@finmars.com>
2020-04-23 16:55:54 +0300

    fix

--- poms_app/custom_logging.py
+++ poms_app/custom_logging.py
@@ -5,13 +5,7 @@ from logging import handlers
 
 class GroupWriteRotatingFileHandler(handlers.RotatingFileHandler):
 
-    def doRollover(self):
-        """
-        Override base class method to make the new log file group writable.
-        """
-        # Rotate the file first.
-        handlers.RotatingFileHandler.doRollover(self)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        # Add group write to the current permissions.
-        currMode = os.stat(self.baseFilename).st_mode
-        os.chmod(self.baseFilename, currMode | stat.S_IWGRP)
+        os.chmod(self.baseFilename, 0o0777)
