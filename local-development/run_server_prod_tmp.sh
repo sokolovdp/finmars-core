46edad7df8eba50139e5151f1d0c36317f506d4a
Sergei Zhitenev <s.zhitenev@finmars.com>
2020-10-19 20:21:23 +0300

    fix

--- local-development/run_server_prod_tmp.sh
+++ local-development/run_server_prod_tmp.sh
@@ -5,7 +5,7 @@ REDIS_HOST=0.0.0.0:6379 \
 DB_NAME=finmars_db \
 DB_USER=root \
 DB_PASSWORD=t564J3T8My \
-DB_HOST=18.197.22.7 \
+DB_HOST=35.158.132.200 \
 DB_PORT=5432 \
 DEBUG=True \
 LOCAL=True \
@@ -16,3 +16,4 @@ SFTP_USERNAME=finmars \
 SFTP_PASSWORD=97cZgv1pL2pz \
 MEDIATOR_URL=http://localhost:8082/ \
 BACKEND_ROLES="ALL" python manage.py runserver
+
