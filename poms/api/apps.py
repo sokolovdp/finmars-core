f08348529d60da7ece2eafc044ba2cf75c90540d
Sergei Zhitenev <s.zhitenev@finmars.com>
2021-04-26 20:10:33 +0300

    fix

--- poms/api/apps.py
+++ poms/api/apps.py
@@ -26,7 +26,8 @@ class ApiConfig(AppConfig):
 
         post_migrate.connect(self.add_view_and_manage_permissions, sender=self)
 
-        post_migrate.connect(self.register_at_authorizer_service, sender=self)
+        if settings.SERVER_TYPE == settings.BACKEND_ROLES.SIMPLE:
+            post_migrate.connect(self.register_at_authorizer_service, sender=self)
 
     def add_view_and_manage_permissions(self, app_config, verbosity=2, using=DEFAULT_DB_ALIAS, **kwargs):
         from poms.common.utils import add_view_and_manage_permissions
