b7591b67cfee8bedf16703e3da413f8cc045986f
Sergei Zhitenev <s.zhitenev@finmars.com>
2021-05-14 11:55:31 +0300

    update

--- poms_app/urls.py
+++ poms_app/urls.py
@@ -39,35 +39,3 @@ if 'django.contrib.admin' in settings.INSTALLED_APPS:
         ]
 
 
-def register_at_authorizer_service():
-
-    import requests
-    import json
-
-    try:
-        print("register_at_authorizer_service processing")
-
-        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-
-        data = {
-            "base_api_url": settings.BASE_API_URL,
-        }
-
-        url = settings.AUTHORIZER_URL + '/backend-is-ready/'
-
-        response = requests.post(url=url, data=json.dumps(data), headers=headers)
-
-        print("register_at_authorizer_service processing response.status_code %s" % response.status_code)
-        print("register_at_authorizer_service processing response.text %s" % response.text)
-
-    except Exception as e:
-        print("register_at_authorizer_service error %s" % e)
-
-def one_time_startup():
-
-    print("On startup methods")
-
-    if 'SIMPLE' in settings.BACKEND_ROLES:
-        register_at_authorizer_service()
-
-one_time_startup()
--- poms_app/wsgi.py
+++ poms_app/wsgi.py
@@ -22,3 +22,38 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poms_app.settings")
 application = get_wsgi_application()
 
 # application = StaticFilesHandler(application)
+
+
+def register_at_authorizer_service():
+
+    import requests
+    import json
+
+    try:
+        print("register_at_authorizer_service processing")
+
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+        data = {
+            "base_api_url": settings.BASE_API_URL,
+        }
+
+        url = settings.AUTHORIZER_URL + '/backend-is-ready/'
+
+        response = requests.post(url=url, data=json.dumps(data), headers=headers)
+
+        print("register_at_authorizer_service processing response.status_code %s" % response.status_code)
+        print("register_at_authorizer_service processing response.text %s" % response.text)
+
+    except Exception as e:
+        print("register_at_authorizer_service error %s" % e)
+
+def one_time_startup():
+
+    print("On startup methods")
+
+    from django.conf import settings
+    if 'SIMPLE' in settings.BACKEND_ROLES:
+        register_at_authorizer_service()
+
+one_time_startup()
