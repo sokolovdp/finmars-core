fffcadb664b71b36076698f3030f2c97f6678fb4
Sergei Zhitenev <s.zhitenev@finmars.com>
2021-05-14 11:37:46 +0300

    update

--- poms/api/apps.py
+++ poms/api/apps.py
@@ -25,31 +25,11 @@ class ApiConfig(AppConfig):
 
         post_migrate.connect(self.add_view_and_manage_permissions, sender=self)
 
-        if 'SIMPLE' in settings.BACKEND_ROLES:
-            post_migrate.connect(self.register_at_authorizer_service, sender=self)
+        # if 'SIMPLE' in settings.BACKEND_ROLES:
+            # post_migrate.connect(self.register_at_authorizer_service, sender=self)
 
     def add_view_and_manage_permissions(self, app_config, verbosity=2, using=DEFAULT_DB_ALIAS, **kwargs):
         from poms.common.utils import add_view_and_manage_permissions
         add_view_and_manage_permissions()
 
 
-    def register_at_authorizer_service(self, app_config, verbosity=2, using=DEFAULT_DB_ALIAS, **kwargs):
-
-        try:
-            _l.info("register_at_authorizer_service processing")
-
-            headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-
-            data = {
-                "base_api_url": settings.BASE_API_URL,
-            }
-
-            url = settings.AUTHORIZER_URL + '/backend-is-ready/'
-
-            response = requests.post(url=url, data=json.dumps(data), headers=headers)
-
-            _l.info("register_at_authorizer_service processing response.status_code %s" % response.status_code)
-            _l.info("register_at_authorizer_service processing response.text %s" % response.text)
-
-        except Exception as e:
-                _l.info("register_at_authorizer_service error %s" % e)
