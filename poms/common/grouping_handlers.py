99aa67655ad88a6e410158dd168661dc1046946e
Sergei Zhitenev <s.zhitenev@finmars.com>
2021-08-25 14:38:40 +0300

    update

--- poms/common/grouping_handlers.py
+++ poms/common/grouping_handlers.py
@@ -149,7 +149,7 @@ def get_root_system_attr_group(qs, root_group, groups_order):
                 .values('group_name', 'group_identifier', 'items_count') \
                 .order_by(root_group)
         else:
-            qs = qs.distinct(root_group) \
+            qs = qs \
                 .annotate(group_name=F(root_group), group_identifier=F(root_group), items_count=Count(root_group)) \
                 .values('group_name', 'group_identifier', 'items_count') \
                 .order_by(root_group)
