1d0953753f06fef2d59dbb7935a7030f4852dfd1
Sergei Zhitenev <s.zhitenev@finmars.com>
2021-09-23 18:16:35 +0300

    update import logic

--- poms/csv_import/tasks.py
+++ poms/csv_import/tasks.py
@@ -1822,32 +1822,28 @@ def handler_instrument_object(source_data, instrument_type, master_user, ecosyst
                                                                user_code=source_data['pricing_currency']).id
     except Exception as e:
 
-        if 'pricing_currency' not in object_data:
-            object_data['pricing_currency'] = ecosystem_default.currency.id
+        object_data['pricing_currency'] = ecosystem_default.currency.id
 
     try:
         object_data['accrued_currency'] = Currency.objects.get(master_user=master_user,
                                                                user_code=source_data['accrued_currency']).id
     except Exception as e:
 
-        if 'accrued_currency' not in object_data:
-            object_data['accrued_currency'] = ecosystem_default.currency.id
+        object_data['accrued_currency'] = ecosystem_default.currency.id
 
     try:
         object_data['payment_size_detail'] = PaymentSizeDetail.objects.get(
             user_code=source_data['payment_size_detail']).id
     except Exception as e:
 
-        if 'payment_size_detail' not in object_data:
-            object_data['payment_size_detail'] = ecosystem_default.payment_size_detail.id
+        object_data['payment_size_detail'] = ecosystem_default.payment_size_detail.id
 
     try:
         object_data['pricing_condition'] = PricingCondition.objects.get(
             user_code=source_data['pricing_condition']).id
     except Exception as e:
 
-        if 'pricing_condition' not in object_data:
-            object_data['pricing_condition'] = ecosystem_default.pricing_condition.id
+        object_data['pricing_condition'] = ecosystem_default.pricing_condition.id
 
 
 
