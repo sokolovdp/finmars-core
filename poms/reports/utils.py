0529a14cf7d74dc144b42a8fcddd9e4dedae6a7f
Sergei Zhitenev <s.zhitenev@finmars.com>
2021-09-28 17:57:58 +0300

    update

--- poms/reports/utils.py
+++ poms/reports/utils.py
@@ -51,27 +51,61 @@ def generate_report_unique_hash(app, action, data, master_user, member):
     if 'pl_first_date' in _data:
         report_options['pl_first_date'] = _data['pl_first_date']
 
-    report_options['report_currency'] = _data['report_currency']
-    report_options['pricing_policy'] = _data['pricing_policy']
-    report_options['report_type'] = _data['report_type']
+    if 'report_currency' in _data:
+        report_options['report_currency'] = _data['report_currency']
 
-    report_options['account_mode'] = _data['account_mode']
-    report_options['portfolio_mode'] = _data['portfolio_mode']
-    report_options['strategy1_mode'] = _data['strategy1_mode']
-    report_options['strategy2_mode'] = _data['strategy2_mode']
-    report_options['strategy3_mode'] = _data['strategy3_mode']
 
-    report_options['custom_fields_to_calculate'] = _data['custom_fields_to_calculate']
-    report_options['cost_method'] = _data['cost_method']
-    report_options['show_balance_exposure_details'] = _data['show_balance_exposure_details']
-    report_options['show_transaction_details'] = _data['show_transaction_details']
-    report_options['approach_multiplier'] = _data['approach_multiplier']
+    if 'pricing_policy' in _data:
+        report_options['pricing_policy'] = _data['pricing_policy']
 
-    report_options['portfolios'] = _data['portfolios']
-    report_options['accounts'] = _data['accounts']
-    report_options['strategies1'] = _data['strategies1']
-    report_options['strategies2'] = _data['strategies2']
-    report_options['strategies3'] = _data['strategies3']
+    if 'report_type' in _data:
+        report_options['report_type'] = _data['report_type']
+
+    if 'account_mode' in _data:
+        report_options['account_mode'] = _data['account_mode']
+
+    if 'portfolio_mode' in _data:
+        report_options['portfolio_mode'] = _data['portfolio_mode']
+
+    if 'strategy1_mode' in _data:
+        report_options['strategy1_mode'] = _data['strategy1_mode']
+
+    if 'strategy2_mode' in _data:
+        report_options['strategy2_mode'] = _data['strategy2_mode']
+
+    if 'strategy3_mode' in _data:
+        report_options['strategy3_mode'] = _data['strategy3_mode']
+
+    if 'custom_fields_to_calculate' in _data:
+        report_options['custom_fields_to_calculate'] = _data['custom_fields_to_calculate']
+
+    if 'cost_method' in _data:
+        report_options['cost_method'] = _data['cost_method']
+
+    if 'show_balance_exposure_details' in _data:
+        report_options['show_balance_exposure_details'] = _data['show_balance_exposure_details']
+
+    if 'show_transaction_details' in _data:
+        report_options['show_transaction_details'] = _data['show_transaction_details']
+
+    if 'approach_multiplier' in _data:
+        report_options['approach_multiplier'] = _data['approach_multiplier']
+
+    if 'portfolios' in _data:
+        report_options['portfolios'] = _data['portfolios']
+
+
+    if 'accounts' in _data:
+        report_options['accounts'] = _data['accounts']
+
+    if 'strategies1' in _data:
+        report_options['strategies1'] = _data['strategies1']
+
+    if 'strategies2' in _data:
+        report_options['strategies2'] = _data['strategies2']
+
+    if 'strategies3' in _data:
+        report_options['strategies3'] = _data['strategies3']
 
     result = app + '_' + action + '_' + str(master_user.id) + '_' + str(member.id) + '_' + hashlib.md5(
         json.dumps(report_options, sort_keys=True).encode('utf-8')).hexdigest()
