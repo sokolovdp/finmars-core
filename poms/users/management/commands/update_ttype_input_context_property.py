cbaed86045849dac62230576bfb52d70c26b1c4f
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-02-01 15:58:27 +0300

    script for ttype inputs

---
+++ poms/users/management/commands/update_ttype_input_context_property.py
@@ -0,0 +1,26 @@
+from django.core.management import BaseCommand
+from django.db import transaction
+
+
+__author__ = 'szhitenev'
+
+
+class Command(BaseCommand):
+    help = 'Add prefix to context property in ttype inputs'
+
+    def handle(self, *args, **options):
+        from poms.transactions.models import TransactionTypeInput
+
+        processed = 0
+
+        for item in TransactionTypeInput.objects.all().iterator():
+
+            if item.context_property is not None:
+
+                if 'context' not in item.context_property:
+                    item.context_property = 'context_' + str(item.context_property)
+                    item.save()
+                    processed = processed + 1
+
+        print('Inputs processed: %s' % processed)
+
