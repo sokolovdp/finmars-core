d0887b2335859087a1573e6e0b9d9065aab2dd3b
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-02-04 17:36:35 +0300

    add flower ampq

--- docker/finmars-run.sh
+++ docker/finmars-run.sh
@@ -82,7 +82,7 @@ then
 
     echo "Run Flower"
 
-    nohup /var/app-venv/bin/celery flower --url-prefix=$BASE_API_URL/flower -A poms_app --broker=amqp://guest:guest@$RABBITMQ_HOST --port=5566 &
+    nohup /var/app-venv/bin/celery -A poms_app --broker=amqp://guest:guest@$RABBITMQ_HOST// flower --url-prefix=$BASE_API_URL/flower --port=5566 &
 
 fi
 
