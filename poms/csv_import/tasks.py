21c646ffd674372db245314a8e9f505f11857394
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-02-04 17:56:01 +0300

    fix import

--- poms/csv_import/tasks.py
+++ poms/csv_import/tasks.py
@@ -1634,6 +1634,7 @@ def data_csv_file_import_by_procedure(self, procedure_instance, transaction_file
 
                         instance = CsvDataFileImport(scheme=scheme,
                                                      file_path=file_path,
+                                                     filename=file_name,
                                                      member=procedure_instance.member,
                                                      master_user=procedure_instance.master_user,
                                                      delimiter=scheme.delimiter,
