76d751bb983f63e8aed081c23810eac668225c68
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-02-04 15:41:15 +0300

    update celery version

--- poms_app/celery.py
+++ poms_app/celery.py
@@ -1,18 +1,18 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import Celery
+import os
 from django.conf import settings
 from poms.common.kombu_serializers import register_pickle_signed
 
 # register_pickle_signed(salt='poms-pickle-signed', compress=True)
 # register_pickle_signed(salt='poms-pickle-signed', compress=False)
 
-app = Celery('poms.backend')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poms_app.settings')
+
+
+app = Celery('poms_app')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
