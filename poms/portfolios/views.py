371bc01685cc322b14cb6de39deaa91756e052f4
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-08-30 11:43:35 +0200

    add keep instrument

--- poms/portfolios/views.py
+++ poms/portfolios/views.py
@@ -291,7 +291,10 @@ class PortfolioRegisterViewSet(AbstractWithObjectPermissionViewSet):
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
 
-        instance.linked_instrument.delete()
+        keep_instrument = request.data.get('keep_instrument')
+
+        if keep_instrument != 'true':
+            instance.linked_instrument.delete()
 
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
