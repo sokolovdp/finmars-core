3f621deef2e67c413ff377d7858c5c10bffba049
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-09-06 15:44:06 +0200

    fix

--- poms/procedures/tasks.py
+++ poms/procedures/tasks.py
@@ -3,6 +3,7 @@ from django.conf import settings
 import requests
 import json
 
+from poms.common.models import ProxyUser, ProxyRequest
 from poms.integrations.tasks import complex_transaction_csv_file_import_by_procedure
 from poms.csv_import.tasks import data_csv_file_import_by_procedure
 
@@ -100,3 +101,40 @@ def procedure_request_data_file(self,
         procedure_instance.save()
 
         raise Exception("Data File Service is unavailable")
+
+
+@shared_task(name='procedures.run_data_procedure_from_formula', bind=True)
+def run_data_procedure_from_formula(self, master_user_id, member_id, user_code,user_context,  **kwargs):
+
+    _l.info('run_data_procedure_from_formula init')
+
+    from poms.users.models import MasterUser
+    from poms.users.models import Member
+    from poms.procedures.models import RequestDataFileProcedure
+
+    master_user = MasterUser.objects.get(id=master_user_id)
+
+    member = Member.objects.get(id=member_id)
+
+    proxy_user = ProxyUser(self.member, self.master_user)
+    proxy_request = ProxyRequest(proxy_user)
+
+    context = {
+        'request': proxy_request
+    }
+
+    merged_context = {}
+    merged_context.update(context)
+
+    if user_context:
+        merged_context['names'].update(user_context)
+
+
+    procedure = RequestDataFileProcedure.objects.get(master_user=master_user, user_code=user_code)
+
+    kwargs.pop('user_context', None)
+
+    from poms.procedures.handlers import RequestDataFileProcedureProcess
+    instance = RequestDataFileProcedureProcess(procedure=procedure, master_user=master_user, member=member,
+                                               context=merged_context, **kwargs)
+    instance.process()
