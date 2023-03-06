5ace004099657219eca867b541c27881a9e368fe
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-03-06 23:23:34 +0100

    fix host url

--- poms/workflows_handler.py
+++ poms/workflows_handler.py
@@ -25,7 +25,7 @@ def get_workflows_list(date_from, date_to):
     headers = {'Content-type': 'application/json', 'Accept': 'application/json',
                'Authorization': 'Bearer %s' % refresh.access_token}
 
-    url = settings.DOMAIN_NAME + '/' + settings.BASE_API_URL + '/workflow/api/workflow/?created_after=' + str(
+    url = 'https://' + settings.DOMAIN_NAME + '/' + settings.BASE_API_URL + '/workflow/api/workflow/?created_after=' + str(
         date_from) + '&created_before=' + str(date_to)
 
     response = requests.get(url, headers=headers)
