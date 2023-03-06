e4b89d463252a56163fa137e866f403200a8cf81
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-03-06 23:28:20 +0100

    fix host url

--- poms/workflows_handler.py
+++ poms/workflows_handler.py
@@ -28,7 +28,7 @@ def get_workflows_list(date_from, date_to):
     url = 'https://' + settings.DOMAIN_NAME + '/' + settings.BASE_API_URL + '/workflow/api/workflow/?created_after=' + str(
         date_from) + '&created_before=' + str(date_to)
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, verify=settings.VERIFY_SSL)
 
     if response.status_code != 200:
         _l.info('get_workflows_list.response.status_code %s' % response.status_code)
