16466163860c8074f7ca6812ea9beae1e32c7750
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-03-09 00:25:43 +0100

    add table size counter

--- poms/api/views.py
+++ poms/api/views.py
@@ -697,8 +697,8 @@ class TablesSizeViewSet(AbstractViewSet):
         query = '''
             select
               table_name,
-              pg_size_pretty(pg_relation_size(quote_ident(table_name))),
-              pg_relation_size(quote_ident(table_name))
+              pg_size_pretty(pg_total_relation_size(quote_ident(table_name))),
+              pg_total_relation_size(quote_ident(table_name))
             from information_schema.tables
             where table_schema = 'public'
             order by 3 desc;
