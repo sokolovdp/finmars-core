b4fa36caab4a2dce2a4c4b7e3d90645ecf104ebb
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-03-09 15:02:52 +0100

    fix filters

--- poms/history/filters.py
+++ poms/history/filters.py
@@ -7,9 +7,34 @@ class HistoryQueryFilter(BaseFilterBackend):
         query = request.query_params.get('query', False)
 
         if query:
-            member = request.user.member
-            return queryset.filter(
-                Q(user_code__icontains=query) | Q(created__icontains=query) | Q(id__icontains=query) | Q(
-                    member__username__icontains=query) | Q(notes__icontains=query) | Q(action__icontains=query) | Q(content_type__model__icontains=query))
+
+            pieces = query.split(' ')
+
+
+            user_code_q = Q()
+            created_q = Q()
+            member_username_q = Q()
+            notes_q = Q()
+            action_q = Q()
+            content_type_q = Q()
+
+            for piece in pieces:
+                user_code_q.add(Q(user_code__icontains=piece), Q.AND)
+                created_q.add(Q(created__icontains=piece), Q.AND)
+                member_username_q.add(Q(member__username__icontains=piece), Q.AND)
+                notes_q.add(Q(notes__icontains=piece), Q.AND)
+                action_q.add(Q(action__icontains=piece), Q.AND)
+                content_type_q.add(Q(content_type__model__icontains=piece), Q.AND)
+
+            options = Q()
+
+            options.add(user_code_q, Q.OR)
+            options.add(created_q, Q.OR)
+            options.add(member_username_q, Q.OR)
+            options.add(notes_q, Q.OR)
+            options.add(action_q, Q.OR)
+            options.add(content_type_q, Q.OR)
+
+            return queryset.filter(options)
 
         return queryset
