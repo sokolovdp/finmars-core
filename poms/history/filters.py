e6853e9182330d9c481cd8ff12840442cb801ebe
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-03-09 23:10:15 +0100

    add context_url to query filter

--- poms/history/filters.py
+++ poms/history/filters.py
@@ -1,8 +1,10 @@
+import logging
+
 from django.db.models import Q
 from rest_framework.filters import BaseFilterBackend
 
-import logging
 _l = logging.getLogger('poms.history')
+
 
 class HistoryQueryFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
@@ -15,17 +17,20 @@ class HistoryQueryFilter(BaseFilterBackend):
             user_code_q = Q()
             created_q = Q()
             notes_q = Q()
+            context_url_q = Q()
 
             for piece in pieces:
                 user_code_q.add(Q(user_code__icontains=piece), Q.AND)
                 created_q.add(Q(created__icontains=piece), Q.AND)
                 notes_q.add(Q(notes__icontains=piece), Q.AND)
+                context_url_q.add(Q(context_url__icontains=piece), Q.AND)
 
             options = Q()
 
             options.add(user_code_q, Q.OR)
             options.add(created_q, Q.OR)
             options.add(notes_q, Q.OR)
+            options.add(context_url_q, Q.OR)
 
             return queryset.filter(options)
 
