2ce8a49d94ef645fa2cf604c52a60dd7763f18bb
dp.sokolov <sokolovdp@gmail.com>
2023-04-06 16:45:02 +0300

    FN-1476 - add db_initializer

--- poms/common/common_base_test.py
+++ poms/common/common_base_test.py
@@ -124,3 +124,178 @@ def show_all_urls():
     django_resolver = get_resolver(None)
     print_urls(django_resolver)
     print("------------------------------------------------")
+
+
+class DbInitializer:
+
+    def create_unified_group(self):
+        return TransactionTypeGroup.objects.filter(
+            name=UNIFIED,
+            user_code=UNIFIED,
+            short_name=UNIFIED,
+        ).first() or TransactionTypeGroup.objects.create(
+            master_user=self.master_user,
+            name=UNIFIED,
+            user_code=UNIFIED,
+            short_name=UNIFIED,
+        )
+
+    def get_or_create_instruments(self) -> dict:
+        self.default_instrument = Instrument.objects.filter(
+            user_code="-",
+        ).first() or Instrument.objects.create(
+            master_user=self.master_user,
+            name="-",
+            user_code="-",
+            short_name="-",
+            public_name="-",
+            instrument_type_id=1,
+            accrued_currency=self.usd,
+            pricing_currency=self.usd,
+            maturity_date=date.today(),
+        )
+
+        instruments = {}
+        for name, type_, class_id in INSTRUMENTS:
+            instrument_type = InstrumentType.objects.filter(
+                name=type_,
+            ).first() or InstrumentType.objects.create(
+                master_user=self.master_user,
+                instrument_class_id=class_id,
+                name=type_,
+                user_code=type_,
+                short_name=type_,
+                public_name=type_,
+            )
+            instrument = Instrument.objects.filter(
+                name=name,
+            ).first() or Instrument.objects.create(
+                master_user=self.master_user,
+                instrument_type=instrument_type,
+                name=name,
+                accrued_currency=self.usd,
+                pricing_currency=self.usd,
+                maturity_date=date.today(),
+            )
+            if not instrument.maturity_date:
+                instrument.maturity_date = date.today()
+                instrument.save()
+
+            instruments[name] = instrument
+        return instruments
+
+    def create_accounts_and_portfolios(self) -> tuple:
+        portfolios = {}
+        accounts = {}
+        for name in PORTFOLIOS:
+            account = Account.objects.filter(
+                name=name
+            ).first() or Account.objects.create(
+                master_user=self.master_user,
+                name=name,
+            )
+            accounts[name] = account
+
+            portfolio = Portfolio.objects.filter(
+                name=name
+            ).first() or Portfolio.objects.create(
+                master_user=self.master_user,
+                name=name,
+            )
+            portfolio.accounts.clear()
+            portfolio.accounts.add(account)
+            portfolio.save()
+
+            portfolios[name] = portfolio
+
+        return accounts, portfolios
+
+    def get_or_create_currencies(self):
+        self.usd = Currency.objects.filter(
+            user_code=USD.name,
+        ).first() or Currency.objects.create(
+            master_user=self.master_user,
+            user_code=USD.name,
+            name=USD.name,
+            default_fx_rate=USD.fx_rate,
+        )
+        self.euro = Currency.objects.filter(
+            user_code=EUR.name,
+        ).first() or Currency.objects.create(
+            master_user=self.master_user,
+            user_code=EUR.name,
+            name=EUR.name,
+            default_fx_rate=EUR.fx_rate,
+        )
+
+    def get_or_create_types(self, master_user) -> dict:
+        types = {}
+        for name in TRANSACTIONS_TYPES:
+            type_obj = TransactionType.objects.filter(
+                user_code=name,
+            ).first()
+            if not type_obj:
+                type_obj = TransactionType.objects.create(
+                    master_user=master_user,
+                    name=name,
+                    user_code=name,
+                    short_name=name,
+                    group=self.group,
+                )
+            types[name] = type_obj
+
+        return types
+
+    def get_or_create_classes(self) -> dict:
+        classes = {}
+        for class_id in TRANSACTIONS_CLASSES:
+            record = TransactionClass.objects.filter(
+                id=class_id
+            ).first() or TransactionClass.objects.create(
+                id=class_id,
+                user_code=random_string(),
+                name=random_string(),
+                short_name=random_string(),
+            )
+            classes[class_id] = record
+        return classes
+
+    def get_or_create_strategies(self):
+        self.strategies = {}
+        for i, model in enumerate([Strategy1, Strategy2, Strategy3]):
+            if not (strategy := model.objects.first()):
+                strategy = model.objects.create(
+                    master_user=self.master_user,
+                    name="1M",
+                )
+            self.strategies[i + 1] = strategy
+
+    def create_counterparties(self):
+        self.counterparty = Counterparty.objects.first()
+        if not self.counterparty:
+            self.counterparty = Counterparty.objects.create(
+                master_user=self.master_user,
+                name="1M",
+                user_code="1M",
+                short_name="1M",
+            )
+
+        self.responsible = Responsible.objects.first()
+        if not self.responsible:
+            self.responsible = Responsible.objects.create(
+                master_user=self.master_user,
+                name="1M",
+                user_code="1M",
+                short_name="1M",
+            )
+
+    def __init__(self):
+        self.master_user = None
+        self.group = self.create_unified_group()
+        self.get_or_create_currencies()
+        self.instruments = self.get_or_create_instruments()
+        self.transaction_classes = self.get_or_create_classes()
+        self.transaction_types = self.get_or_create_types(self.master_user)
+        self.accounts, self.portfolios = self.create_accounts_and_portfolios()
+        self.get_or_create_strategies()
+        self.create_counterparties()
