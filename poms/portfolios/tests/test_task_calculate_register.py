5ffdecefae94fe48d4ac9921c2a769e3bb6a62c3
dp.sokolov <sokolovdp@gmail.com>
2023-04-14 16:06:40 +0300

    FN-1476 fix test_portfolio_register_views.py and add test_task_calculate_register.py

---
+++ poms/portfolios/tests/test_task_calculate_register.py
@@ -0,0 +1,18 @@
+# from datetime import date
+# from django.conf import settings
+# from django.contrib.auth.models import User
+#
+# from poms.common.common_base_test import BaseTestCase, BIG
+# from poms.portfolios.tasks import calculate_portfolio_register_record
+# from poms.portfolios.models import PortfolioRegisterRecord
+# from poms.transactions.models import TransactionClass
+# from poms.users.models import Member
+#
+#
+# class CalculatePortfolioRegisterRecordTaskTest(BaseTestCase):
+#     def setUp(self):
+#         super().setUp()
+#         self.init_test_case()
+#
+#     def test_no_portfolios(self):
+#         pass
