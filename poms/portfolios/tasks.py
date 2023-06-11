c8f39f76d166f19989dca305b290db1a85a6aae7
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-06-11 02:02:20 +0200

    fix

--- poms/portfolios/tasks.py
+++ poms/portfolios/tasks.py
@@ -541,12 +541,10 @@ def calculate_portfolio_register_price_history(self, task_id):
                     result[portfolio_register.user_code]["dates"] = []
                     continue
 
-            result["date_from"] = _date_from
-            result["date_to"] = date_to
+            result[portfolio_register.user_code]["date_from"] = _date_from
+            result[portfolio_register.user_code]["date_to"] = date_to
 
-            result[portfolio_register.user_code][
-                "dates"
-            ] = get_list_of_dates_between_two_dates(_date_from, date_to)
+            result[portfolio_register.user_code]["dates"] = get_list_of_dates_between_two_dates(_date_from, date_to)
 
             if not portfolio_register.linked_instrument:
                 result[portfolio_register.user_code]["error_message"] = (
@@ -569,7 +567,7 @@ def calculate_portfolio_register_price_history(self, task_id):
             true_pricing_policy = portfolio_register.valuation_pricing_policy
 
             _l.info('going calculate %s' % portfolio_register)
-            _l.info('going calculate _date_from %s' % result["date_from"])
+            _l.info('going calculate date_from %s' % result["date_from"])
             _l.info('going calculate date_to %s' % result["date_to"])
             _l.info('going calculate dates len %s' % len(item["dates"]))
 
