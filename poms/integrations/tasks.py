70a1c8d5e1017a12296c1ff0cd752b1b3d081ec9
dp.sokolov <sokolovdp@gmail.com>
2023-06-16 19:44:26 +0300

    FN-1736 fix error in tasks.py

--- poms/integrations/tasks.py
+++ poms/integrations/tasks.py
@@ -359,7 +359,7 @@ def create_instrument_from_finmars_database(data, master_user, member):
     instrument_data = {
         key: None if value == "null" else value for key, value in data.items()
     }
-    short_type = instrument_data["instrument_type"]["type_user_code"]
+    short_type = instrument_data["instrument_type"]["user_code"]
     try:
         # TODO remove stocks ASAP as configuration ready
         if short_type in {"stocks", "stock"}:
@@ -405,8 +405,8 @@ def create_instrument_from_finmars_database(data, master_user, member):
         try:
             instrument_type = InstrumentType.objects.get(
                 master_user=master_user,
-                user_code=instrument_type_user_code,
-                # user_code__contains=short_type,  # FOR DEBUG ONLY!
+                # user_code=instrument_type_user_code,
+                user_code__contains=short_type,  # FOR DEBUG ONLY!
             )
         except InstrumentType.DoesNotExist as e:
             # all_types = InstrumentType.objects.all().values_list(
@@ -906,8 +906,8 @@ def create_simple_instrument(task: CeleryTask) -> Optional[Instrument]:
     try:
         instrument_type = InstrumentType.objects.get(
             master_user=task.master_user,
-            user_code=instrument_type_user_code_full,
-            # user_code__contains=type_user_type,  # FOR DEBUG ONLY!
+            # user_code=instrument_type_user_code_full,
+            user_code__contains=type_user_type,  # FOR DEBUG ONLY!
         )
     except InstrumentType.DoesNotExist:
         err_msg = (
