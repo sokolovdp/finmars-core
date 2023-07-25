4c610369cf4aebae068320526101b487933d2b32
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-07-25 14:26:19 +0200

    add celery global error handler

--- poms_app/celery.py
+++ poms_app/celery.py
@@ -1,7 +1,7 @@
 import os
 
 from celery import Celery
-
+from celery.signals import task_failure
 # from django.conf import settings
 # from poms.common.kombu_serializers import register_pickle_signed
 # register_pickle_signed(salt='poms-pickle-signed', compress=True)
@@ -16,3 +16,35 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.task_routes = {"*": {"queue": "backend-general-queue"}}
+
+
+@task_failure.connect
+def handle_task_failure(**kwargs):
+
+    import logging
+    from poms.celery_tasks.models import CeleryTask
+    _l = logging.getLogger('celery')
+
+    try:
+        exception = kwargs['exception']
+        task_id = kwargs['task_id']
+        args = kwargs['args']
+        kwargs = kwargs['kwargs']
+        traceback = kwargs['traceback']
+        einfo = kwargs['einfo']
+
+        # Handle the exception in any way you want. For example, you could log it:
+        _l.error(f'Task {task_id} raised exception: {einfo.exception} \n {einfo.traceback}')
+
+        try:
+
+            task = CeleryTask.objects.get(celery_task_id=task_id)
+            task.error_message = einfo.exception
+            task.status = CeleryTask.STATUS_ERROR
+            task.save()
+
+        except Exception as e:
+            _l.error("Task is not registered in CeleryTask %s" % e)
+
+    except Exception as e:
+        _l.error("Could not handle task failure %s" % e)
