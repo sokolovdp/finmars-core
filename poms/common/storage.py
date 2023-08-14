1977433fba6eb8a85f4b51c4687450afd00744c1
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-08-14 12:51:29 +0200

    fix

--- poms/common/storage.py
+++ poms/common/storage.py
@@ -137,6 +137,25 @@ class FinmarsStorage(EncryptedStorage):
     To ensure that storage overwrite passed filepath insead of appending a number to it
     '''
 
+    def save(self, name, content, max_length=None):
+        """
+        Save new content to the file specified by name. The content should be
+        a proper File object or any Python file-like object, ready to be read
+        from the beginning.
+        """
+        # Get the proper name for the file, as it will actually be saved.
+        if name is None:
+            name = content.name
+
+        if not hasattr(content, "chunks"):
+            content = File(content, name)
+
+        name = self.get_available_name(name, max_length=max_length)
+        name = self._save(name, content)
+        # Ensure that the name returned from the storage system is still valid.
+        # validate_file_name(name, allow_relative_path=True) # TODO Not needed
+        return name
+
     def get_available_name(self, name, max_length=None):
         self.delete(name)
         return super().get_available_name(name, max_length)
