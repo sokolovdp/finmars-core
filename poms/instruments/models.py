d2abfd0a26c6593f7d8a3a3b217462e5996707b0
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-09-25 21:33:40 +0200

    FN-2181 fix accrued price

--- poms/instruments/models.py
+++ poms/instruments/models.py
@@ -3000,8 +3000,10 @@ class PriceHistory(DataTimeStampedModel):
 
         if not self.accrued_price:
             try:
-                self.accrued_price = self.instrument.get_accrued_price(date)
+                self.accrued_price = self.instrument.get_accrued_price(self.date)
             except Exception as e:
+                _l.error('PriceHistory.error get_accrued_price e %s' % e)
+                _l.error('PriceHistory.error get_accrued_price traceback %s' % traceback.format_exc())
                 _l.error('PriceHistory cound not get_accrued_price')
                 self.accrued_price = 0
 
