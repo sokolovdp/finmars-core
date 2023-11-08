12f5bf3402b64d3759b2650a32a75564efc15593
Dmitrii Sokolov <d.sokolov@finmars.com>
2023-11-08 17:09:16 +0000

    FN-2297 - Invite function fails

--- poms/system_messages/serializers.py
+++ poms/system_messages/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
-from poms.system_messages.models import SystemMessage, SystemMessageAttachment, SystemMessageComment
+from poms.system_messages.models import (
+    SystemMessage,
+    SystemMessageAttachment,
+    SystemMessageComment,
+)
 from poms.users.fields import MasterUserField
 from poms.users.utils import get_member_from_context
 
@@ -8,13 +12,22 @@ from poms.users.utils import get_member_from_context
 class SystemMessageAttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = SystemMessageAttachment
-        fields = ('id', 'file_url', 'file_name', 'notes', 'file_report')
+        fields = (
+            "id",
+            "file_url",
+            "file_name",
+            "notes",
+            "file_report",
+        )
 
     def __init__(self, *args, **kwargs):
-        super(SystemMessageAttachmentSerializer, self).__init__(*args, **kwargs)
-
         from poms.file_reports.serializers import FileReportSerializer
-        self.fields['file_report_object'] = FileReportSerializer(source='file_report', read_only=True)
+
+        super().__init__(*args, **kwargs)
+
+        self.fields["file_report_object"] = FileReportSerializer(
+            source="file_report", read_only=True
+        )
 
 
 class SystemMessageCommentSerializer(serializers.ModelSerializer):
@@ -22,57 +35,65 @@ class SystemMessageCommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SystemMessageComment
-        fields = ('id', 'member', 'member_object', 'comment', 'created', 'modified')
+        fields = (
+            "id",
+            "member",
+            "member_object",
+            "comment",
+            "created",
+            "modified",
+        )
 
     def get_member_object(self, instance):
-        return {
-            'id': instance.member.id,
-            'username': instance.member.username
-        }
+        return {"id": instance.member.id, "username": instance.member.username}
 
 
 class SystemMessageSerializer(serializers.ModelSerializer):
     master_user = MasterUserField()
-
     attachments = SystemMessageAttachmentSerializer(many=True, read_only=True)
     comments = SystemMessageCommentSerializer(many=True, read_only=True)
 
     class Meta:
-
         model = SystemMessage
-        fields = ('id', 'master_user',
-                  'section', 'type', 'action_status',
-                  'title', 'description',
-                  'created',
-                  'linked_event',
-                  'performed_by', 'created',
-                  'comments',
-                  'attachments')
+        fields = (
+            "id",
+            "master_user",
+            "section",
+            "type",
+            "action_status",
+            "title",
+            "description",
+            "created",
+            "linked_event",
+            "performed_by",
+            "created",
+            "comments",
+            "attachments",
+        )
 
     def to_representation(self, instance):
-
         member = get_member_from_context(self.context)
 
-        result = super(SystemMessageSerializer, self).to_representation(instance)
+        result = super().to_representation(instance)
 
         for member_message in instance.members.all():
-
             if member_message.member_id == member.id:
-                result['is_read'] = member_message.is_read
-                result['is_pinned'] = member_message.is_pinned
+                result["is_read"] = member_message.is_read
+                result["is_pinned"] = member_message.is_pinned
 
-        if 'is_read' not in result:
-            result['is_read'] = True
+        if "is_read" not in result:
+            result["is_read"] = True
 
-        if 'is_pinned' not in result:
-            result['is_pinned'] = False
-
-        # _l.debug('InstrumentLightSerializer done: %s', "{:3.3f}".format(time.perf_counter() - st))
+        if "is_pinned" not in result:
+            result["is_pinned"] = False
 
         return result
 
 
 class SystemMessageActionSerializer(serializers.Serializer):
-    ids = serializers.PrimaryKeyRelatedField(many=True, queryset=SystemMessage.objects.all())
-    sections = serializers.MultipleChoiceField(default=SystemMessage.SECTION_OTHER,
-                                               choices=SystemMessage.SECTION_CHOICES)
+    ids = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=SystemMessage.objects.all()
+    )
+    sections = serializers.MultipleChoiceField(
+        default=SystemMessage.SECTION_OTHER, choices=SystemMessage.SECTION_CHOICES
+    )
---
+++ poms/system_messages/tests/test_views.py
@@ -0,0 +1,94 @@
+from django.conf import settings
+
+from poms.common.common_base_test import BaseTestCase
+from poms.system_messages.models import SystemMessage, SystemMessageMember
+
+API_URL = (
+    f"/{settings.BASE_API_URL}/api/v1/system-messages/message/?"
+    f"page_size=100&created_after=2023-11-6&action_status=2,3&type=2,3"
+)
+REQUEST_PARAMS = {
+    "page_size": 100,
+    "created_after": "2023-11-06",
+    "action_status": "2,3",
+    "type": "2,3",
+}
+
+
+class MemberViewSetTest(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.init_test_case()
+        self.url = API_URL
+
+    def create_system_message(self, title: str) -> SystemMessage:
+        system_message = SystemMessage.objects.create(
+            master_user=self.master_user,
+            type=SystemMessage.TYPE_ERROR,
+            action_status=SystemMessage.ACTION_STATUS_REQUIRED,
+            title=title,
+            description=self.random_string(),
+        )
+        SystemMessageMember.objects.create(
+            system_message=system_message,
+            member=self.member,
+        )
+        return system_message
+
+    def test__check_api_url(self):
+        response = self.client.get(path=self.url, data=REQUEST_PARAMS)
+        self.assertEqual(response.status_code, 200, response.content)
+
+    @BaseTestCase.cases(
+        ("workflow", "workflow"),
+        ("Workflow_failed.", "Workflow_failed."),
+        ("No_WorkFlow", "No_WorkFlow"),
+        ("WORKFLOW_IS_BIG", "WORKFLOW_IS_BIG"),
+    )
+    def test__list_workflow_excluded(self, title):
+        self.create_system_message(title)
+        self.create_system_message("Normal flow")
+
+        self.assertEqual(SystemMessage.objects.all().count(), 2)
+
+        response = self.client.get(path=self.url)
+        self.assertEqual(response.status_code, 200, response.content)
+
+        response_json = response.json()
+        self.assertEqual(response_json["count"], 1)
+
+    @BaseTestCase.cases(
+        ("workflow", "workflow"),
+        ("Workflow_failed.", "Workflow_failed."),
+        ("No_WorkFlow", "No_WorkFlow"),
+        ("WORKFLOW_IS_BIG", "WORKFLOW_IS_BIG"),
+    )
+    def test__list_workflow_all_excluded(self, title):
+        self.create_system_message(title)
+        self.create_system_message(title)
+
+        self.assertEqual(SystemMessage.objects.all().count(), 2)
+
+        response = self.client.get(path=self.url)
+        self.assertEqual(response.status_code, 200, response.content)
+
+        response_json = response.json()
+        self.assertEqual(response_json["count"], 0)
+
+    @BaseTestCase.cases(
+        ("workflo-w", "workflo-w"),
+        ("Worflow_failed.", "Worflow_failed."),
+        ("No_Worklow", "No_Worklow"),
+        ("WORKFLO_IS_BIG", "WORKFLO_IS_BIG"),
+    )
+    def test__list_no_workflow(self, title):
+        self.create_system_message(title)
+        self.create_system_message(title)
+
+        self.assertEqual(SystemMessage.objects.all().count(), 2)
+
+        response = self.client.get(path=self.url)
+        self.assertEqual(response.status_code, 200, response.content)
+
+        response_json = response.json()
+        self.assertEqual(response_json["count"], 2)
