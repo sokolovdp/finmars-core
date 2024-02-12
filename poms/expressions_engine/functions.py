192a3b6c4ccfaefb2369267fc9f75143c78658ee
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-02-12 21:51:49 +0100

    PLAT-397 result = result * -1 sync with 1.5.0-rc

--- poms/expressions_engine/functions.py
+++ poms/expressions_engine/functions.py
@@ -3206,6 +3206,8 @@ def _get_net_cost_price_on_date(
         if total_position != 0:
             result = total_principal / total_position
 
+            result = result * -1 # get price with the right sign
+
         return result
 
     except Exception as e:
