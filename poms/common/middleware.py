212f857ed62cc0451d1b5ad451f3dd1d678fe7ac
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-03-24 14:03:25 +0100

    PLAT-535 add RealmAndSpaceMiddleware

--- poms/common/middleware.py
+++ poms/common/middleware.py
@@ -6,7 +6,7 @@ import re
 import time
 import uuid
 from threading import local
-
+from django.db import connection
 from django.conf import settings
 from django.contrib.gis.geoip2 import GeoIP2
 from django.http.response import JsonResponse
@@ -355,6 +355,14 @@ class ResponseTimeMiddleware(MiddlewareMixin):
 
         return response
 
+def schema_exists(schema_name):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT schema_name
+            FROM information_schema.schemata
+            WHERE schema_name = %s;
+        """, [schema_name])
+        return cursor.fetchone() is not None
 
 class RealmAndSpaceMiddleware:
     def __init__(self, get_response):
@@ -368,7 +376,36 @@ class RealmAndSpaceMiddleware:
         if 'realm' in path_parts[1]:
             request.realm_code = path_parts[1]
             request.space_code = path_parts[2]
+
+            if not schema_exists(request.space_code):
+
+                # Uncomment in 1.9.0 when there is no more legacy Spaces
+                # Handle the error (e.g., log it, return a 400 Bad Request, etc.)
+                # For demonstration, returning a simple HttpResponseBadRequest
+                # return HttpResponseBadRequest("Invalid space code.")
+
+                with connection.cursor() as cursor:
+                    cursor.execute(f"SET search_path TO public;")
+
+            else:
+
+                # Setting the PostgreSQL search path to the tenant's schema
+                with connection.cursor() as cursor:
+                    cursor.execute(f"SET search_path TO {request.space_code};")
+
         else:
+            # If we do not have realm_code, we suppose its legacy Space which do not need scheme changing
             request.space_code = path_parts[1]
 
-        return self.get_response(request)
+            # Remain in public scheme
+            with connection.cursor() as cursor:
+                cursor.execute("SET search_path TO public;")
+
+        response = self.get_response(request)
+
+        # Optionally, reset the search path to default after the request is processed
+        # This can be important in preventing "leakage" of the schema setting across requests
+        with connection.cursor() as cursor:
+            cursor.execute("SET search_path TO public;")
+
+        return response
