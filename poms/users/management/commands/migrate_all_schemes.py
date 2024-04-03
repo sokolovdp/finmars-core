893765cd4080048f5c3eedce301ca16d8bc89fb9
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-04-04 01:49:06 +0200

    PLAT-556 migrate all migrations

--- poms/users/management/commands/migrate_all_schemes.py
+++ poms/users/management/commands/migrate_all_schemes.py
@@ -28,16 +28,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for schema in get_all_tenant_schemas():
 
-            if 'public' not in schema:
-                self.stdout.write(self.style.SUCCESS(f"Applying migrations to {schema}..."))
+            self.stdout.write(self.style.SUCCESS(f"Applying migrations to {schema}..."))
 
-                # Set the search path to the tenant's schema
-                with connection.cursor() as cursor:
-                    cursor.execute(f"SET search_path TO {schema};")
+            # Set the search path to the tenant's schema
+            with connection.cursor() as cursor:
+                cursor.execute(f"SET search_path TO {schema};")
 
-                # Programmatically call the migrate command
-                call_command('migrate', *args, **options)
+            # Programmatically call the migrate command
+            call_command('migrate', *args, **options)
 
-                # Optionally, reset the search path to default after migrating
-                with connection.cursor() as cursor:
-                    cursor.execute("SET search_path TO public;")
+            # Optionally, reset the search path to default after migrating
+            with connection.cursor() as cursor:
+                cursor.execute("SET search_path TO public;")
