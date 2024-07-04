3756863c909d261fc4bdad603176f2bbbe2033b3
Dmitrii Sokolov <d.sokolov@finmars.com>
2024-07-04 18:22:24 +0000

    PLAT-689 simple import creates duplicated accrual schedules

---
+++ poms/instruments/tests/test_accrual_schedule.py
@@ -0,0 +1,101 @@
+from datetime import date
+
+from poms.common.common_base_test import BaseTestCase
+from poms.instruments.models import (
+    AccrualCalculationModel,
+    AccrualCalculationSchedule,
+    Instrument,
+    DATE_FORMAT,
+)
+
+ACCRUAL_MODELS_IDS = [
+    AccrualCalculationModel.DAY_COUNT_ACT_ACT_ICMA,
+    AccrualCalculationModel.DAY_COUNT_ACT_ACT_ISDA,
+    AccrualCalculationModel.DAY_COUNT_ACT_360,
+    AccrualCalculationModel.DAY_COUNT_ACT_365L,
+    AccrualCalculationModel.DAY_COUNT_30_360_ISDA,
+    AccrualCalculationModel.DAY_COUNT_NL_365,
+    AccrualCalculationModel.DAY_COUNT_30_360_US,
+    AccrualCalculationModel.DAY_COUNT_BD_252,
+    AccrualCalculationModel.DAY_COUNT_30_360_GERMAN,
+    AccrualCalculationModel.DAY_COUNT_30E_PLUS_360,
+    AccrualCalculationModel.DAY_COUNT_ACT_365_FIXED,
+    AccrualCalculationModel.DAY_COUNT_30E_360,
+    AccrualCalculationModel.DAY_COUNT_ACT_365A,
+    AccrualCalculationModel.DAY_COUNT_ACT_366,
+    AccrualCalculationModel.DAY_COUNT_ACT_364,
+]
+
+
+class AccrualCalculationScheduleModelTest(BaseTestCase):
+    databases = "__all__"
+    maxDiff = None
+
+    def setUp(self):
+        super().setUp()
+        self.init_test_case()
+        self.instrument = Instrument.objects.first()
+        self.accrual_model_id = self.random_choice(ACCRUAL_MODELS_IDS)
+
+    @BaseTestCase.cases(
+        ("1", date.today(), date.today()),
+        ("2", date.today().strftime(DATE_FORMAT), date.today().strftime(DATE_FORMAT)),
+        ("3", date.today().strftime(DATE_FORMAT), date.today()),
+        ("4", date.today(), date.today().strftime(DATE_FORMAT)),
+    )
+    def test_create_date_or_str(self, accrual_start_date, first_payment_date):
+        AccrualCalculationSchedule.objects.create(
+            instrument=self.instrument,
+            accrual_calculation_model_id=self.accrual_model_id,
+            accrual_start_date=accrual_start_date,
+            first_payment_date=first_payment_date,
+        )
+
+    @BaseTestCase.cases(
+        ("empty", ""),
+        ("null", None),
+        ("int", 172651),
+        ("str", "18736"),
+    )
+    def test_error_invalid_start_date(self, accrual_start_date):
+        with self.assertRaises(Exception):
+            AccrualCalculationSchedule.objects.create(
+                instrument=self.instrument,
+                accrual_calculation_model_id=self.accrual_model_id,
+                accrual_start_date=accrual_start_date,
+                first_payment_date=self.random_future_date(),
+            )
+
+    @BaseTestCase.cases(
+        ("empty", ""),
+        ("null", None),
+        ("int", 827634),
+        ("str", "827634"),
+    )
+    def test_error_invalid_first_payment_date(self, first_payment_date):
+        with self.assertRaises(Exception):
+            AccrualCalculationSchedule.objects.create(
+                instrument=self.instrument,
+                accrual_calculation_model_id=self.accrual_model_id,
+                accrual_start_date=self.random_future_date(),
+                first_payment_date=first_payment_date
+            )
+
+    @BaseTestCase.cases(
+        ("date", date.today()),
+        ("str", date.today().strftime(DATE_FORMAT)),
+    )
+    def test_error_duplicated_start_date(self, accrual_start_date):
+        AccrualCalculationSchedule.objects.create(
+            instrument=self.instrument,
+            accrual_calculation_model_id=self.accrual_model_id,
+            accrual_start_date=accrual_start_date,
+            first_payment_date=self.random_future_date()
+        )
+        with self.assertRaises(Exception):
+            AccrualCalculationSchedule.objects.create(
+                instrument=self.instrument,
+                accrual_calculation_model_id=self.accrual_model_id,
+                accrual_start_date=accrual_start_date,
+                first_payment_date=self.random_future_date()
+            )
