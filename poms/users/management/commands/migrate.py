9d9636771b8683d17d5b62eec841f125d94655f3
Anton Alekseev <a.alekseev@finmars.com>
2024-07-24 11:12:53 +0000

    PLAT-967 --space-code in "migrate" and "shell" commands

---
+++ poms/users/management/commands/migrate.py
@@ -0,0 +1,14 @@
+from django.core.management.commands.migrate import Command as OriginalMigrateCommand
+from django.db import connection
+
+
+class Command(OriginalMigrateCommand):
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument("--space-code", help="Workspace code (DB schema)")
+
+    def handle(self, *args, **options):
+        if options["space_code"]:
+            with connection.cursor() as cursor:
+                cursor.execute(f"SET search_path TO {options['space_code']};")
+        super().handle(*args, **options)
