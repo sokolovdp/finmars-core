53d0e2ab33ffc75154ff78b5a732cbf3717edda1
Sergei Zhitenev <s.zhitenev@finmars.com>
2024-08-08 22:43:41 +0200

    PLAT-955 fix integration

--- poms/vault/vault.py
+++ poms/vault/vault.py
@@ -33,7 +33,7 @@ class FinmarsVault():
         self.auth_token = None
         try:
             vault_token = VaultRecord.objects.get(user_code='hashicorp-vault-token')
-            self.auth_token = json.loads(vault_token.data["token"])
+            self.auth_token = json.loads(vault_token.data)["token"]
         except Exception as e:
             _l.info(f'Failed to get vault token: {e}')
 
