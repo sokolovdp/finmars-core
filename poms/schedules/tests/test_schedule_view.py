a7214b8c9302da3e3189b87b3d241aa5e97cf8fe
Yaroslav Kluchnikov <y.kluchnikov@finmars.com>
2024-11-13 20:40:39 +0300

    refactored

--- poms/schedules/tests/test_schedule_view.py
+++ poms/schedules/tests/test_schedule_view.py
@@ -10,8 +10,6 @@ class ScheduleViewSetTest(BaseTestCase):
         super().setUp()
         self.init_test_case()
 
-        self.realm_code = 'realm00000'
-        self.space_code = 'space00000'
         self.url = f"/{self.realm_code}/{self.space_code}/api/v1/schedules/schedule/"
         self.schedule = self.create_schedule()
 
