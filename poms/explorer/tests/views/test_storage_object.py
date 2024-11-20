93165bb4bc28c48ede1d3cd99bb3362136f10ec7
dp.sokolov <sokolovdp@gmail.com>
2024-11-20 22:22:21 +0300

    PLAT-870 update tests

--- poms/explorer/tests/views/test_storage_object.py
+++ poms/explorer/tests/views/test_storage_object.py
@@ -83,38 +83,6 @@ class StorageObjectResourceGroupViewTest(BaseTestCase):
 
         self.assertEqual(len(directory_data["resource_groups_object"]), 1)
 
-    def test_update_resource_groups(self):
-        name_1 = self.random_string()
-        self.create_group(name=name_1)
-        name_2 = self.random_string()
-        self.create_group(name=name_2)
-        name_3 = self.random_string()
-        self.create_group(name=name_3)
-
-        response = self.client.patch(
-            f"{self.url}{self.directory.id}/",
-            data={"resource_groups": [name_1, name_2, name_3]},
-            format="json",
-        )
-        self.assertEqual(response.status_code, 200, response.content)
-
-        directory_data = response.json()
-        self.assertEqual(len(directory_data["resource_groups"]), 3)
-        self.assertEqual(len(directory_data["resource_groups_object"]), 3)
-
-        response = self.client.patch(
-            f"{self.url}{self.directory.id}/",
-            data={"resource_groups": [name_2]},
-            format="json",
-        )
-        self.assertEqual(response.status_code, 200, response.content)
-
-        directory_data = response.json()
-        self.assertEqual(len(directory_data["resource_groups"]), 1)
-        self.assertEqual(directory_data["resource_groups"], [name_2])
-
-        self.assertEqual(len(directory_data["resource_groups_object"]), 1)
-
     def test_remove_resource_groups(self):
         name_1 = self.random_string()
         self.create_group(name=name_1)
