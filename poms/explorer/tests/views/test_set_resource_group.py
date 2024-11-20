85850d270092d63962b4661bcf1e7209dbc42502
dp.sokolov <sokolovdp@gmail.com>
2024-11-20 20:29:46 +0300

    PLAT-1330 update tests

--- poms/explorer/tests/views/test_set_resource_group.py
+++ poms/explorer/tests/views/test_set_resource_group.py
@@ -47,3 +47,35 @@ class StorageObjectResourceGroupViewTest(BaseTestCase):
         so_data = response.json()
         self.assertIn("resource_groups", so_data)
         self.assertEqual(so_data["resource_groups"], [rg.name])
+
+    def test_update_resource_groups(self):
+        name_1 = self.random_string()
+        self.create_group(name=name_1)
+        name_2 = self.random_string()
+        self.create_group(name=name_2)
+        name_3 = self.random_string()
+        self.create_group(name=name_3)
+
+        response = self.client.patch(
+            f"{self.url}{self.directory.id}/",
+            data={"resource_groups": [name_1, name_2, name_3]},
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200, response.content)
+
+        directory_data = response.json()
+        self.assertEqual(len(directory_data["resource_groups"]), 3)
+        self.assertEqual(len(directory_data["resource_groups_object"]), 3)
+
+        response = self.client.patch(
+            f"{self.url}{self.directory.id}/",
+            data={"resource_groups": [name_2]},
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200, response.content)
+
+        directory_data = response.json()
+        self.assertEqual(len(directory_data["resource_groups"]), 1)
+        self.assertEqual(directory_data["resource_groups"], [name_2])
+
+        self.assertEqual(len(directory_data["resource_groups_object"]), 1)
