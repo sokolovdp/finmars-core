5a72377a83e8f46d13fb1241218ceb13ee833bbf
dp.sokolov <sokolovdp@gmail.com>
2025-03-18 00:25:03 +0300

    PLAT-1752 update settings.py

--- poms_app/settings.py
+++ poms_app/settings.py
@@ -674,19 +674,20 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
 AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", None)
 AWS_S3_VERIFY = os.environ.get("AWS_S3_VERIFY", None)
 AWS_S3_SIGNATURE_VERSION = "s3v4"
-AWS_S3_CLIENT_CONFIG = {
-    "config": {
-        "retries": {
-            "max_attempts": 3,
-            "mode": "standard",
-        },
-        "paginator_config": {
-            "PageSize": 500,
-        },
-        "connect_timeout": 30,
-        "read_timeout": 180,
-    },
-}
+# AWS_S3_CLIENT_CONFIG = {
+#     "signature_version": "s3v4",
+#     "config": {
+#         "retries": {
+#             "max_attempts": 3,
+#             "mode": "standard",
+#         },
+#         "paginator_config": {
+#             "PageSize": 500,
+#         },
+#         "connect_timeout": 30,
+#         "read_timeout": 180,
+#     },
+# }
 if os.environ.get("AWS_S3_VERIFY") == "False":
     AWS_S3_VERIFY = False
 
